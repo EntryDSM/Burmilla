1a48e3391a6fb46507e22ec66289e21e82131739
Hyeonyee <jgh10070@naver.com>
2021-06-08 16:58:52 +0900

    [FIX]

--- src/data/modules/store.ts
+++ src/data/modules/store.ts
@@ -1,4 +1,4 @@
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import createSagaMiddleware from 'redux-saga';
 
