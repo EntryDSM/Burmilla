94bf8faa8147fdfd4c50399c68152735fb990e2c
Hyeonyee <jgh10070@naver.com>
2021-06-10 08:31:34 +0900

    [FIX] Main 추가

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -2,17 +2,28 @@ import React, { FC } from "react";
 import * as S from "./style";
 import MainContent from "./content";
 import Progress from "./progress";
+import { statusType } from "../../data/modules/redux/reducer/status/mainConstance";
+import { useHistory } from "react-router-dom";
 
 interface Props {
+  status?: statusType;
   date?: string;
 }
 
-const Main: FC<Props> = ({ date }) => {
+const Main: FC<Props> = ({ status, date }) => {
+  const history = useHistory();
+  const mainButtonClickHandler = () => {
+    history.push("/");
+  };
   return (
     <S.Main>
       <S.MainBackground />
       <S.MainWrapper>
-        <MainContent date={date} />
+        <MainContent
+          {...process}
+          buttonClickHandler={mainButtonClickHandler}
+          date={date}
+        />
       </S.MainWrapper>
       <Progress />
     </S.Main>
