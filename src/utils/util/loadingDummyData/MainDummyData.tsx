72e303eddeb0f9058820078c2ae0c826a3062f04
Hyeonyee <jgh10070@naver.com>
2021-06-10 23:04:54 +0900

    [ADD] MainDummyData 추가

---
+++ src/utils/util/loadingDummyData/MainDummyData.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { processType } from "../../../data/modules/redux/reducer/status/interface";
+
+const MainDummyData: processType = {
+  title: <p>로딩중...</p>,
+  getDescription: () => <p>로딩중...</p>,
+  isHaveTerm: false,
+  isButtonAble: false,
+  buttonText: "로딩중...",
+  uri: "",
+};
+
+export default MainDummyData;
