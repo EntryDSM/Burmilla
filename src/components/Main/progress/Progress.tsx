fb85a9ebf8500724f81a6296583825e27800a3df
Hyeonyee <jgh10070@naver.com>
2021-06-10 23:10:24 +0900

    [FIX] Progress Props interface 추가, status에서 Reducer 받아옴

--- src/components/Main/progress/Progress.tsx
+++ src/components/Main/progress/Progress.tsx
@@ -1,19 +1,59 @@
 import React, { FC } from "react";
 import * as S from "../style";
 import ProgressContent from "./ProgressContent";
+import {
+  FIRST_ANNOUNCEMENT,
+  INTERVIEW,
+  mainProcessNumber,
+  SECOND_ANNOUNCEMENT,
+  START_DATE,
+  statusType,
+} from "../../../data/modules/redux/reducer/status/mainConstance";
 
-interface Props {}
+interface Props {
+  status: statusType;
+  date: string;
+}
 
-const Progress: FC<Props> = ({}) => {
+const SECOND_DATE = 86400000;
+
+const Progress: FC<Props> = ({ status, date }) => {
+  const getLeftDate = (date: string): number => {
+    const dateObj = new Date(date);
+    const nowDateObj = new Date();
+    const leftSecond = dateObj.getTime() - nowDateObj.getTime();
+    return Math.floor(leftSecond / SECOND_DATE);
+  };
   return (
     <S.Progress>
-      <ProgressContent progressName="원서 작성" />
+      <ProgressContent
+        progressName="원서 작성"
+        isNow={status === START_DATE}
+        isPassed={mainProcessNumber[status] >= 2}
+        leftDate={getLeftDate(date)}
+      />
       <S.ProgressBlankBar />
-      <ProgressContent progressName="1차 발표" />
+      <ProgressContent
+        progressName="1차 발표"
+        isNow={status === FIRST_ANNOUNCEMENT}
+        isPassed={mainProcessNumber[status] >= 4}
+        leftDate={getLeftDate(date)}
+      />
       <S.ProgressBlankBar />
-      <ProgressContent progressName="면접" />
+      <ProgressContent
+        progressName="면접"
+        isNow={status === INTERVIEW}
+        isPassed={mainProcessNumber[status] >= 5}
+        leftDate={getLeftDate(date)}
+      />
       <S.ProgressBlankBar />
-      <ProgressContent progressName="발표 및 등록" />
+      <ProgressContent
+        progressName="발표 및 등록"
+        isNow={status === SECOND_ANNOUNCEMENT}
+        isPassed={mainProcessNumber[status] >= 6}
+        leftDate={getLeftDate(date)}
+      />
+      <div />
     </S.Progress>
   );
 };
