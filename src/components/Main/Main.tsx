165171d6114cec16718997f192600304d9fc002d
Hyeonyee <jgh10070@naver.com>
2021-06-10 23:10:42 +0900

    [FIX] Main: Propgress 추가

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -2,15 +2,17 @@ import React, { FC } from "react";
 import * as S from "./style";
 import MainContent from "./content";
 import Progress from "./progress";
+import { processType } from "../../data/modules/redux/reducer/status/interface";
 import { statusType } from "../../data/modules/redux/reducer/status/mainConstance";
 import { useHistory } from "react-router-dom";
 
 interface Props {
-  status?: statusType;
-  date?: string;
+  status: statusType;
+  process: processType;
+  date: string;
 }
 
-const Main: FC<Props> = ({ status, date }) => {
+const Main: FC<Props> = ({ status, process, date }) => {
   const history = useHistory();
   const mainButtonClickHandler = () => {
     history.push("/");
@@ -24,8 +26,8 @@ const Main: FC<Props> = ({ status, date }) => {
           buttonClickHandler={mainButtonClickHandler}
           date={date}
         />
+        <Progress status={status} date={date} />
       </S.MainWrapper>
-      <Progress />
     </S.Main>
   );
 };
