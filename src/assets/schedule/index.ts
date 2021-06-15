72d049ef5df53852d974c12658470ff2ec84a251
Hyeonyee <jgh10070@naver.com>
2021-06-15 23:54:50 +0900

    [FIX] schedule

--- src/assets/schedule/index.ts
+++ src/assets/schedule/index.ts
@@ -2,4 +2,5 @@ export { default as application_icon } from './schedule_modify_application.svg';
 export { default as interview_icon } from './schedule_modify_interview.svg';
 export { default as presentation_regiester_icon } from './schedule_modify_presentation_registration.svg';
 export { default as presentation_icon } from './schedule_modify_presentation.svg';
-export { default as schedule_shift_icon } from './schedule_modify_shift.svg';
+export { default as select_off } from './schedule_modify_select_off.svg';
+export { default as select_on } from './schedule_modify_select_on.svg';
