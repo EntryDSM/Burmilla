b3a7bcf1ac32e22643b5a7ea8a5e473b6d7af172
Hyeonyee <jgh10070@naver.com>
2021-06-17 20:03:16 +0900

    [FIX] header style

--- src/components/Header/style.ts
+++ src/components/Header/style.ts
@@ -1,23 +1,34 @@
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
-import { color } from '../../styles/index';
+import { color, pxToRem } from '../../styles/index';
 
-export const HeaderContainer = styled.div`
-
-`;
 
 export const HeaderWrapper = styled.header`
   width: 100%;
+  min-width: ${pxToRem(1320)}rem;
   height: 60px;
   box-shadow: 0 4px 6px 0 rgba(0, 0, 0, 0.12);
   padding: 0 260px;
   box-sizing: border-box;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
   background: white;
   position: absolute;
   z-index: 1;
+`;
+
+export const HeaderContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const HeaderLogo = styled.div`
+  width: 15%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
 `;
 
 export const Logo = styled.img`
@@ -25,20 +36,74 @@ export const Logo = styled.img`
   height: 29px;
 `;
 
-export const HeaderItemsContainer = styled.div`
+export const HeaderButton = styled.div`
+  width: 45%;
+  height: 100%;
   display: flex;
   align-items: center;
+`;
 
-  &>Button {
-    margin-right:15px;
+export const HeaderItemsContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-evenly;
+`;
+
+export const HeaderLogoutBox = styled.div`
+  width: 16%;
+  display: flex;
+  justify-content: center;
+
+  > Button {
+    color: ${color.primaryTextColor};
     font-weight: bold;
     font-size: 15px;
   }
 `;
 
+export const HeaderItemBox = styled.div`
+  width: 17%;
+  &:nth-child(1) {
+    width: 8%;
+  }
+  &:nth-child(3) {
+    width: 21%;
+  }
+`;
+
 export const HeaderItem = styled(Link)`
+    width: 100%;
     color: ${color.primaryTextColor};
-    font-weight: 600;
+    font-weight: bold;
     font-size: 15px;
-    margin-right: 15px;
+
+    > div {
+      height: 2px;
+      width: 1px;
+      visibility: hidden;
+      position: absolute;
+      background-color: ${color.primaryColor};
+      transition: all 0.3s;
+    }
+
+    &:hover > .schedule {
+      width: 1.8%;
+      height: 2px;
+      visibility: visible;
+      align-self: flex-start;
+    }
+    &:hover > .total {
+      width: 3.9%;
+      height: 2px;
+      visibility: visible;
+      align-self: flex-start;
+    }
+    &:hover > .applicant {
+      width: 4.8%;
+      height: 2px;
+      visibility: visible;
+      align-self: flex-start;
+    }
 `;
