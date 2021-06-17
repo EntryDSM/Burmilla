43e4f6a3077965dbf14671dbac1a966127031e57
Hyeonyee <jgh10070@naver.com>
2021-06-17 20:04:16 +0900

    [FIX] page 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,6 @@
 import * as React from "react";
 import { BrowserRouter, Redirect, Route, Switch } from "react-router-dom";
 
-// import Login from "./page/Login";
-// import Applicant from "./page/Applicant";
-// import Message from "./page/Message";
 import { MainContainer, ScheduleContainer, LoginContainer } from "./containers";
 import CheckToken from "./components/Common/CheckToken";
 import { useHeader } from "./hooks/default";
@@ -19,7 +16,6 @@ function App() {
           <Route exact path="/" component={MainContainer} />
           {/*<Route exact path="/applicant" component={MainContainer} />*/}
           <Route exact path="/schedule" component={ScheduleContainer} />
-          {/*<Route exact path="/message" component={MainContainer} />*/}
           <Route component={() => <Redirect to="/" />} />
         </CheckToken>
       </Switch>
