345d0b5dd102c64c42d515544d40e93ec418a02e
Hyeonyee <jgh10070@naver.com>
2021-06-18 23:04:46 +0900

    [FIX] Schedule select 수정

--- src/components/Schedule/content/ScheduleContent.tsx
+++ src/components/Schedule/content/ScheduleContent.tsx
@@ -1,5 +1,6 @@
 import React, { FC } from "react";
 import * as S from "../style";
+import { monthDate, dayDate } from "./scheduleConstance";
 
 interface Props {
   scheduleName: string;
@@ -12,69 +13,71 @@ const ScheduleContent: FC<Props> = ({
   scheduleImg,
   scheduleImgAlt,
 }) => {
-  const scheduleSelectBox = () => {
-    return (
-      <S.ScheduleSelect>
-        <S.ScheduleDateSelect>
-          <S.ScheduleSelectOptions>1</S.ScheduleSelectOptions>
-          <S.ScheduleSelectOptions>2</S.ScheduleSelectOptions>
-          <S.ScheduleSelectOptions>3</S.ScheduleSelectOptions>
-          <S.ScheduleSelectOptions>4</S.ScheduleSelectOptions>
-          <S.ScheduleSelectOptions>5</S.ScheduleSelectOptions>
-          <S.ScheduleSelectOptions>6</S.ScheduleSelectOptions>
-          <S.ScheduleSelectOptions>7</S.ScheduleSelectOptions>
-          <S.ScheduleSelectOptions>8</S.ScheduleSelectOptions>
-          <S.ScheduleSelectOptions>9</S.ScheduleSelectOptions>
-          <S.ScheduleSelectOptions>10</S.ScheduleSelectOptions>
-          <S.ScheduleSelectOptions>11</S.ScheduleSelectOptions>
-          <S.ScheduleSelectOptions>12</S.ScheduleSelectOptions>
-        </S.ScheduleDateSelect>
-        <span>월</span>
-        <S.ScheduleDateSelect>
-          <S.ScheduleSelectOptions>1</S.ScheduleSelectOptions>
-          <S.ScheduleSelectOptions>2</S.ScheduleSelectOptions>
-          <S.ScheduleSelectOptions>3</S.ScheduleSelectOptions>
-          <S.ScheduleSelectOptions>4</S.ScheduleSelectOptions>
-          <S.ScheduleSelectOptions>5</S.ScheduleSelectOptions>
-          <S.ScheduleSelectOptions>6</S.ScheduleSelectOptions>
-          <S.ScheduleSelectOptions>7</S.ScheduleSelectOptions>
-          <S.ScheduleSelectOptions>8</S.ScheduleSelectOptions>
-          <S.ScheduleSelectOptions>9</S.ScheduleSelectOptions>
-          <S.ScheduleSelectOptions>10</S.ScheduleSelectOptions>
-          <S.ScheduleSelectOptions>11</S.ScheduleSelectOptions>
-          <S.ScheduleSelectOptions>12</S.ScheduleSelectOptions>
-          <S.ScheduleSelectOptions>13</S.ScheduleSelectOptions>
-          <S.ScheduleSelectOptions>14</S.ScheduleSelectOptions>
-          <S.ScheduleSelectOptions>15</S.ScheduleSelectOptions>
-          <S.ScheduleSelectOptions>16</S.ScheduleSelectOptions>
-          <S.ScheduleSelectOptions>17</S.ScheduleSelectOptions>
-          <S.ScheduleSelectOptions>18</S.ScheduleSelectOptions>
-          <S.ScheduleSelectOptions>19</S.ScheduleSelectOptions>
-          <S.ScheduleSelectOptions>20</S.ScheduleSelectOptions>
-          <S.ScheduleSelectOptions>21</S.ScheduleSelectOptions>
-          <S.ScheduleSelectOptions>22</S.ScheduleSelectOptions>
-          <S.ScheduleSelectOptions>23</S.ScheduleSelectOptions>
-          <S.ScheduleSelectOptions>24</S.ScheduleSelectOptions>
-          <S.ScheduleSelectOptions>25</S.ScheduleSelectOptions>
-          <S.ScheduleSelectOptions>26</S.ScheduleSelectOptions>
-          <S.ScheduleSelectOptions>27</S.ScheduleSelectOptions>
-          <S.ScheduleSelectOptions>28</S.ScheduleSelectOptions>
-          <S.ScheduleSelectOptions>29</S.ScheduleSelectOptions>
-          <S.ScheduleSelectOptions>30</S.ScheduleSelectOptions>
-          <S.ScheduleSelectOptions>31</S.ScheduleSelectOptions>
-        </S.ScheduleDateSelect>
-        <span>일</span>
-      </S.ScheduleSelect>
+  const [monthDatas, setMonthDatas] = React.useState([]);
+  const [dayDatas, setDayDatas] = React.useState([]);
+
+  React.useEffect(() => {
+    setMonthDatas(
+      monthDate.map((monthInfo) => ({
+        month: monthInfo.month,
+      }))
     );
-  };
+
+    setDayDatas(
+      dayDate.map((dayInfo) => ({
+        day: dayInfo.day,
+      }))
+    );
+  }, []);
+
   const dateSelectOption = () => {
     if (scheduleName === "원서 작성") {
       return (
         <>
           <S.ScheduleDateTitle>시작일</S.ScheduleDateTitle>
-          {scheduleSelectBox()}
-          <S.ScheduleDateTitle>마감일</S.ScheduleDateTitle>
-          {scheduleSelectBox()}
+          <S.ScheduleDateSelectBox>
+            <S.ScheduleSelect>
+              <S.ScheduleDateSelect>
+                {monthDatas &&
+                  monthDatas.map((months) => {
+                    return <p>{months.month}</p>;
+                  })}
+              </S.ScheduleDateSelect>
+              <span>월</span>
+            </S.ScheduleSelect>
+            <S.ScheduleSelect>
+              <S.ScheduleDateSelect>
+                {dayDatas &&
+                  dayDatas.map((days) => {
+                    return <p>{days.day}</p>;
+                  })}
+              </S.ScheduleDateSelect>
+              <span>일</span>
+            </S.ScheduleSelect>
+          </S.ScheduleDateSelectBox>
+          <S.ScheduleDateTitle className="dead-line">
+            마감일
+          </S.ScheduleDateTitle>
+          <S.ScheduleDateSelectBox>
+            <S.ScheduleSelect>
+              <S.ScheduleDateSelect>
+                {monthDatas &&
+                  monthDatas.map((months) => {
+                    return <p>{months.month}</p>;
+                  })}
+              </S.ScheduleDateSelect>
+              <span>월</span>
+            </S.ScheduleSelect>
+            <S.ScheduleSelect>
+              <S.ScheduleDateSelect>
+                {dayDatas &&
+                  dayDatas.map((days) => {
+                    return <p>{days.day}</p>;
+                  })}
+              </S.ScheduleDateSelect>
+              <span>일</span>
+            </S.ScheduleSelect>
+          </S.ScheduleDateSelectBox>
         </>
       );
     } else {
@@ -83,7 +86,26 @@ const ScheduleContent: FC<Props> = ({
           <S.ScheduleDateTitle className="scheduleSelectBox-tail">
             시작일
           </S.ScheduleDateTitle>
-          {scheduleSelectBox()}
+          <S.ScheduleDateSelectBox>
+            <S.ScheduleSelect>
+              <S.ScheduleDateSelect>
+                {monthDatas &&
+                  monthDatas.map((months) => {
+                    return <p>{months.month}</p>;
+                  })}
+              </S.ScheduleDateSelect>
+              <span>월</span>
+            </S.ScheduleSelect>
+            <S.ScheduleSelect>
+              <S.ScheduleDateSelect>
+                {dayDatas &&
+                  dayDatas.map((days) => {
+                    return <p>{days.day}</p>;
+                  })}
+              </S.ScheduleDateSelect>
+              <span>일</span>
+            </S.ScheduleSelect>
+          </S.ScheduleDateSelectBox>
         </>
       );
     }
@@ -96,9 +118,7 @@ const ScheduleContent: FC<Props> = ({
           <img src={scheduleImg} alt={scheduleImgAlt} />
         </S.ScheduleImg>
         <S.ScheduleDate>
-          <S.ScheduleDateSelectBox>
-            {dateSelectOption()}
-          </S.ScheduleDateSelectBox>
+          <S.ScheduleDateBody>{dateSelectOption()}</S.ScheduleDateBody>
         </S.ScheduleDate>
       </S.ScheduleContentBox>
     </S.ScheduleContent>
