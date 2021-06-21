4aa1d0e4d04a1e49101788fd9f1362c01f3f85b0
Hyeonyee <jgh10070@naver.com>
2021-06-21 23:59:40 +0900

    [FIX] Statistics

---
+++ src/components/Statistics/style.ts
@@ -0,0 +1,35 @@
+import styled from 'styled-components';
+
+export const StatisticsPageContainer = styled.div`
+  width: 100%;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: #e0faff;
+  position: relative;
+`;
+
+export const StatisticsContainer = styled.div`
+
+`;
+
+export const StatisticsBox = styled.div`
+
+`;
+
+export const StatisticsTitle = styled.h1`
+
+`;
+
+export const StatisticsSubTitle = styled.p`
+
+`;
+
+export const CompetitionTableWrapper = styled.div`
+
+`;
+
+export const TotalScore = styled.div`
+
+`;
