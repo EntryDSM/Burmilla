d4a94ecef3a8919547dfc8ab857d5e38fb73d05e
Hyeonyee <jgh10070@naver.com>
2021-06-23 00:00:39 +0900

    [FIX] CompetitionTotalGraph

--- src/components/Statistics/CompetitionTotalGraph/CompetitionTotalGraph.tsx
+++ src/components/Statistics/CompetitionTotalGraph/CompetitionTotalGraph.tsx
@@ -1,11 +1,43 @@
 import React, { FC } from "react";
+import * as S from "../style";
+import { useStatistics } from "../../../hooks/statistics";
+import { GraphRateItems } from "./CompetitionGraphs/GraphRateItems";
+import { CommonGraph, MeisterGraph, SocialGraph } from "./CompetitionGraphs";
 
-const CompetitionView: FC = () => {
+const CompetitionTotalGraph: FC = () => {
+  const {
+    statisticsStore: {
+      statistics: { total_applicant_count, total_competition_rate },
+    },
+    getStatistics,
+  } = useStatistics();
+
+  React.useEffect(() => {
+    // getStatistics({ area: "all" });
+  }, []);
+
   return (
-    <div>
-      <div></div>
-    </div>
+    <S.CompetitionTotalGraphWrapper>
+      <S.CompetitionFreshmanTotal>
+        <span>총계:</span>
+        <S.TotalApplicantScore>{total_applicant_count}명</S.TotalApplicantScore>
+        <span>경쟁율:</span>
+        <S.TotalCompetitionRate>
+          {total_competition_rate}:1
+        </S.TotalCompetitionRate>
+      </S.CompetitionFreshmanTotal>
+      <S.CompetitionGraphContainer>
+        <CommonGraph />
+        <MeisterGraph />
+        <SocialGraph />
+      </S.CompetitionGraphContainer>
+      <S.GraphRateBar>
+        {GraphRateItems.map((item) => (
+          <S.GraphItemBox>{item.content}</S.GraphItemBox>
+        ))}
+      </S.GraphRateBar>
+    </S.CompetitionTotalGraphWrapper>
   );
 };
 
-export default CompetitionView;
+export default CompetitionTotalGraph;
