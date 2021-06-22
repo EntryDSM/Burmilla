cad565d4544a277de2bef8f84cf87fa9741d031b
Hyeonyee <jgh10070@naver.com>
2021-06-23 00:00:23 +0900

    [FIX] CompetitionView Function Components 추가

--- src/components/Statistics/CompetitionView/CompetitionView.tsx
+++ src/components/Statistics/CompetitionView/CompetitionView.tsx
@@ -1,11 +1,37 @@
 import React, { FC } from "react";
+import * as S from "../style";
+import { useStatistics } from "../../../hooks/statistics";
 
-const CompetitionTotalGraph: FC = () => {
+const CompetitionView: FC = () => {
+  const {
+    statisticsStore: {
+      statistics: { total_applicant_count, total_competition_rate },
+    },
+    getStatistics,
+  } = useStatistics();
+
+  React.useEffect(() => {
+    // getStatistics({ area: "all" });
+  }, []);
+
   return (
-    <div>
-      <div></div>
-    </div>
+    <S.CompetitionViewWrapper className="no-select">
+      <tbody>
+        <tr>
+          <S.CompetitionViewTH>지원자 수 : </S.CompetitionViewTH>
+          <S.CompetitionViewTD>
+            <b>{total_applicant_count}</b> 명
+          </S.CompetitionViewTD>
+        </tr>
+        <tr>
+          <S.CompetitionViewTH>경쟁률 : </S.CompetitionViewTH>
+          <S.CompetitionViewTD>
+            <b>{total_competition_rate} : 1</b>
+          </S.CompetitionViewTD>
+        </tr>
+      </tbody>
+    </S.CompetitionViewWrapper>
   );
 };
 
-export default CompetitionTotalGraph;
+export default CompetitionView;
