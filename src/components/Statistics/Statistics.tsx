9fef4e159f74cd3f5fef2529fd109282f8751385
Hyeonyee <jgh10070@naver.com>
2021-06-22 23:59:45 +0900

    [FIX] Statistics.tsx

--- src/components/Statistics/Statistics.tsx
+++ src/components/Statistics/Statistics.tsx
@@ -11,6 +11,14 @@ const Statistics: FC = () => {
         <S.StatisticsBox>
           <S.StatisticsTitle>입학원서 접수 현황</S.StatisticsTitle>
           <S.StatisticsSubTitle>Entry DSM 2021 Admin page</S.StatisticsSubTitle>
+          <CompetitionView />
+          <S.CompetitionTableTitle>
+            2022년 신입생 지원률
+          </S.CompetitionTableTitle>
+          <CompetitionTotalGraph />
+          <S.CompetitionTableTitle>
+            2022년 신입생 환산점수
+          </S.CompetitionTableTitle>
           <CompetitionTable />
         </S.StatisticsBox>
       </S.StatisticsContainer>
