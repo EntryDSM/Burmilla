86632f98a592a786fa75cb43cb966335b200b66e
Hyeonyee <jgh10070@naver.com>
2021-06-24 00:08:33 +0900

    [FIX] Graph state 추가

--- src/components/Statistics/CompetitionTotalGraph/CompetitionGraphs/MeisterGraph/index.tsx
+++ src/components/Statistics/CompetitionTotalGraph/CompetitionGraphs/MeisterGraph/index.tsx
@@ -3,6 +3,8 @@ import * as S from "../../../style";
 import { useStatistics } from "../../../../../hooks/statistics";
 
 const MeisterGraph: FC = () => {
+  const [isWidthOfMeisterGraphBar, setIsWidthOfMeisterGraphBar] =
+    React.useState<any>();
   const {
     statisticsStore: {
       statistics: { meister_applicant },
@@ -13,12 +15,27 @@ const MeisterGraph: FC = () => {
   React.useEffect(() => {
     // getStatistics({ area: "all" });
   }, []);
+  React.useEffect(() => {
+    setIsWidthOfMeisterGraphBar(meister_applicant.applicant_count);
+  }, []);
+
+  const graphBarContent = () => {
+    if (isWidthOfMeisterGraphBar >= 1) {
+      return (
+        <S.GraphBarContentText>
+          {isWidthOfMeisterGraphBar}
+        </S.GraphBarContentText>
+      );
+    }
+  };
 
   return (
     <S.GraphWrapper>
       <S.GraphTitle>마이스터</S.GraphTitle>
       <S.GraphChart>
-        <S.GraphBar></S.GraphBar>
+        <S.MeisterGraphBar isWidthOfMeisterGraphBar={isWidthOfMeisterGraphBar}>
+          {graphBarContent()}
+        </S.MeisterGraphBar>
         <S.GraphCompetitionRate>
           {meister_applicant.competition_rate}:1
         </S.GraphCompetitionRate>
