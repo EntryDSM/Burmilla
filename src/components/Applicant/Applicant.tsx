c41da561ce61d562b68d34297c6575370d790123
Hyeonyee <jgh10070@naver.com>
2021-06-25 00:01:15 +0900

    [ADD] Applicant

---
+++ src/components/Applicant/Applicant.tsx
@@ -0,0 +1,33 @@
+import React, { FC } from "react";
+import * as S from "./style";
+import ApplicantInfo from "./ApplicantInfo";
+import ApplicantsList from "./ApplicantsList";
+import FilterSearch from "./FilterSearch";
+import Pagination from "./Pagination";
+import { useApplicant } from "../../hooks/applicant";
+
+const Applicant: FC = () => {
+  const {
+    applicantStore: { currnetApplicantInfo, filters },
+    getApplicantsList,
+  } = useApplicant();
+
+  React.useEffect(() => {
+    // getApplicantsList(filters);
+  }, [filters]);
+
+  return (
+    <S.Applicant>
+      <S.ApplicantContainer>
+        <FilterSearch />
+        <ApplicantsList />
+        <Pagination />
+      </S.ApplicantContainer>
+      <S.ApplicantInfoWrap>
+        {currnetApplicantInfo && <ApplicantInfo />}
+      </S.ApplicantInfoWrap>
+    </S.Applicant>
+  );
+};
+
+export default Applicant;
