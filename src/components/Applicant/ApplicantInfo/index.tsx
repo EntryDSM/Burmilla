aece328f1f36720cc28e8a23ec4414db12c8c99e
Hyeonyee <jgh10070@naver.com>
2021-06-25 00:01:11 +0900

    [ADD] ApplicantInfo

---
+++ src/components/Applicant/ApplicantInfo/index.tsx
@@ -0,0 +1,32 @@
+import * as React from "react";
+
+import Unsubmitted from "./Unsubmitted";
+import Submitted from "./Submitted";
+import { useApplicant } from "../../../hooks/applicant";
+
+function ApplicantInfo() {
+  const {
+    applicantStore: {
+      currnetApplicantInfo: {
+        applicant_information,
+        applicant_contact,
+        school_tel,
+      },
+    },
+  } = useApplicant();
+
+  return applicant_information ? (
+    <Submitted
+      applicantPrivacy={applicant_information.privacy}
+      applicantEvaluation={applicant_information.evaluation}
+      applicantStatus={applicant_information.status}
+    />
+  ) : (
+    <Unsubmitted
+      applicant_contact={applicant_contact}
+      school_tel={school_tel || null}
+    />
+  );
+}
+
+export default ApplicantInfo;
