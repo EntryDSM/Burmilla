68a7d443d5965f9a4d9df673a454091fb48f0315
Hyeonyee <jgh10070@naver.com>
2021-06-24 23:56:25 +0900

    [ADD] Applicant FilterSearch style

---
+++ src/components/Applicant/FilterSearch/style.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+const FilterSearchWrapper = styled.div`
+  width: 100%;
+  height: 14%;
+  display: flex;
+  flex-direction: column;
+  padding: 2% 6%;
+  box-sizing: border-box;
+  justify-content: flex-end;
+  position: relative;
+`;
+
+export default FilterSearchWrapper;
