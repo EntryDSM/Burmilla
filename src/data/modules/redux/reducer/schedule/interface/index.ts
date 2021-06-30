11c70cb4b5372ac7e8315ae59a77906c3f8f6925
Hyeonyee <jgh10070@naver.com>
2021-06-30 22:27:59 +0900

    [FIX] schedule interface

--- src/data/modules/redux/reducer/schedule/interface/index.ts
+++ src/data/modules/redux/reducer/schedule/interface/index.ts
@@ -1,9 +1,32 @@
 import { scheduleType } from '../scheduleConstance';
 
 interface IScheduleState {
+    updateScheduleStatusStatus: 0 | 204 | 400 | 401 | 403 | 404;
     status: scheduleType;
-    monthDate: string;
-    dayDate: string;
+    isStart: boolean;
+    isEnd: boolean;
+    processes: Object;
+    date: Array<UpdateScheduleProcessTimeType>;
 }  
+
+export interface UpdateScheduleProcessTimeType {
+    type: string;
+    startDate: Array<startDateType>;
+    endDate?: Array<endDateType>;
+}
+
+export interface startDateType {
+    month: string;
+    day: string;
+}
+
+export interface endDateType {
+    month: string;
+    day: string;
+}
+
+export interface scheduleProcessType {
+    title: React.ReactNode;
+}
 
 export default IScheduleState;
