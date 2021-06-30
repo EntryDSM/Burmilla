cc3bc00db8a068df0e603d6b75b2b17eadf3f852
Hyeonyee <jgh10070@naver.com>
2021-06-30 22:27:40 +0900

    [FIX] scheduleConstance Record 추가

--- src/data/modules/redux/reducer/schedule/scheduleConstance.tsx
+++ src/data/modules/redux/reducer/schedule/scheduleConstance.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { scheduleProcessType } from "./interface";
 
 export const START_DATE = "START_DATE" as const;
 export const FIRST_ANNOUNCEMENT = "FIRST_ANNOUNCEMENT" as const;
@@ -11,6 +12,19 @@ export type scheduleType =
   | typeof INTERVIEW
   | typeof SECOND_ANNOUNCEMENT;
 
-const scheduleConstance = {};
+const scheduleConstance: Record<scheduleType, scheduleProcessType> = {
+  [START_DATE]: {
+    title: <p>원서 작성</p>,
+  },
+  [FIRST_ANNOUNCEMENT]: {
+    title: <p>1차 발표</p>,
+  },
+  [INTERVIEW]: {
+    title: <p>면접</p>,
+  },
+  [SECOND_ANNOUNCEMENT]: {
+    title: <p>발표 및 등록</p>,
+  },
+};
 
 export default scheduleConstance;
