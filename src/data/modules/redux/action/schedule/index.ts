f44d53bd36d0a179aa706bac474e26f626597515
Hyeonyee <jgh10070@naver.com>
2021-06-30 22:30:25 +0900

    [FIX] schedule action

--- src/data/modules/redux/action/schedule/index.ts
+++ src/data/modules/redux/action/schedule/index.ts
@@ -1,23 +1,84 @@
-import { scheduleType } from '../../reducer/schedule/scheduleConstance'
-import { STATUS, MONTH_DATE, DAY_DATE } from './interface';
+import { UpdateScheduleProcessTimeType } from '../../reducer/schedule/interface';
+import { scheduleType } from '../../reducer/schedule/scheduleConstance';
+import {
+    STATUS,
+    STATUS_SUCCESS,
+    STATUS_FAILURE,
+    IS_START,
+    IS_END,
+    PROCESS,
+    GET_STATUS,
+    UPDATE_SCHEDULE_STATUS,
+    UPDATE_SCHEDULE_STATUS_ASYNC
+} from './interface';
+import {
+    ApiPayload,
+} from "../../../../api/apiTypes";
 
 export const setStatus = (payload: scheduleType) => ({
     type: STATUS,
     payload
 })
 
-export const setMonthDate = (payload: string) => ({
-    type: MONTH_DATE,
-    payload,
+export const setProcess = (payload: Object) => ({
+    type: PROCESS,
+    payload
 })
 
-export const setDayDate = (payload: string) => ({
-    type: DAY_DATE,
-    payload,
-})
+export const setIsStart = (payload: boolean) => ({
+  type: IS_START,
+  payload,
+});
+  
+export const setIsEnd = (payload: boolean) => ({
+  type: IS_END,
+  payload,
+});
 
-export { STATUS, MONTH_DATE, DAY_DATE };
+export const getStatusSuccess = (payload: {
+  schedules: Array<UpdateScheduleProcessTimeType>;
+  current_status: string;
+}) => ({
+  type: STATUS_SUCCESS,
+  payload,
+});
+  
+export const getStatusFailure = (payload: number) => ({
+  type: STATUS_FAILURE,
+  payload,
+});
+
+export const getStatus = () => ({
+    type: GET_STATUS,
+});
+
+export type updateScheduleStatusType =
+  | typeof UPDATE_SCHEDULE_STATUS
+  | typeof UPDATE_SCHEDULE_STATUS_ASYNC;
+
+export type updateScheduleStatusPayload = ApiPayload &
+  UpdateScheduleProcessTimeType;
+
+export interface UpdateScheduleStatus {
+  type: updateScheduleStatusType;
+  payload: updateScheduleStatusPayload;
+}
+
+export const updateScheduleStatusAction = (
+  payload: updateScheduleStatusPayload,
+): UpdateScheduleStatus => ({
+  type: UPDATE_SCHEDULE_STATUS,
+  payload,
+});
+
+export { IS_START, IS_END, STATUS, PROCESS };
 export type scheduleActionType = 
   | ReturnType<typeof setStatus>
-  | ReturnType<typeof setMonthDate>
-  | ReturnType<typeof setDayDate>
+  | ReturnType<typeof setProcess>
+  | ReturnType<typeof setIsStart>
+  | ReturnType<typeof setIsEnd>
+  | ReturnType<typeof getStatusSuccess>
+  | ReturnType<typeof getStatusFailure>
+  | ReturnType<typeof getStatus>
+  | ReturnType<typeof updateScheduleStatusAction>
+  | UpdateScheduleStatus;
