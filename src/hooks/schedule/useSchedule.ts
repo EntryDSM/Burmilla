9aae0db6827f8a0e6e2d42890580826fc8608ca3
Hyeonyee <jgh10070@naver.com>
2021-06-30 22:26:41 +0900

    [FIX] 잠깐 주석처리

--- src/hooks/schedule/useSchedule.ts
+++ src/hooks/schedule/useSchedule.ts
@@ -1,24 +1,41 @@
-import { useDispatch } from 'react-redux';
-import { useSelectState } from '../default';
-import {
-    setStatus,
-    setMonthDate,
-    setDayDate
-  } from '../../data/modules/redux/action/schedule';
-import { scheduleType } from '../../data/modules/redux/reducer/schedule/scheduleConstance'
+// import { useCallback } from 'react';
+// import { useDispatch } from 'react-redux';
+// import { useSelectState } from '../default';
+// import {
+//     setStatus,
+//     setIsStart,
+//     setIsEnd,
+//     setProcess,
+//     getStatus,
+//     getStatusSuccess,
+//     updateScheduleStatusPayload,
+//     updateScheduleStatusAction
+//   } from '../../data/modules/redux/action/schedule';
+// import { UpdateScheduleProcessTimeType, scheduleProcessType } from "../../data/modules/redux/reducer/schedule/interface";
+// import { scheduleType } from '../../data/modules/redux/reducer/schedule/scheduleConstance'
 
-const useSchedule = () => {
-    const dispatch = useDispatch();
-    const state = useSelectState().schedule;
-    const setState = {
-        setStatus: (payload: scheduleType) => dispatch(setStatus(payload)),
-        setMonthDate: (payload: string) => dispatch(setMonthDate(payload)),
-        setDayDate: (payload: string) => dispatch(setDayDate(payload)),
-    };
-    return {
-        state,
-        setState
-    }
-}
+// const useSchedule = () => {
+// const dispatch = useDispatch();
+//   const state = useSelectState().schedule;
+//   const setState = {
+//     setStatus: (payload: scheduleType) => dispatch(setStatus(payload)),
+//     setIsStart: (payload: boolean) => dispatch(setIsStart(payload)),
+//     setIsEnd: (payload: boolean) => dispatch(setIsEnd(payload)),
+//     setProcess: (payload: scheduleProcessType[]) => dispatch(setProcess(payload)),
+//     getStatus: () => dispatch(getStatus()),
+//     setStatusSuccess: (payload: { schedules: Array<UpdateScheduleProcessTimeType>; current_status: string }) =>
+//       dispatch(getStatusSuccess(payload)),
+//   };
+//   const updateScheduleStatus = useCallback(
+//     (payload: updateScheduleStatusPayload) =>
+//       dispatch(updateScheduleStatusAction(payload)),
+//     [dispatch],
+//   );
+//   return {
+//       state,
+//       setState,
+//       updateScheduleStatus,
+//   };
+// };
 
-export default useSchedule;
+// export default useSchedule;
