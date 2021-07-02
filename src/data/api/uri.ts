acd728212da0594a4675f489d48ace2dc1d3e3de
Hyeonyee <jgh10070@naver.com>
2021-07-02 22:34:28 +0900

    [FIX] 모든 uri에 /admin 추가

--- src/data/api/uri.ts
+++ src/data/api/uri.ts
@@ -1,13 +1,13 @@
 enum uri {
-    signin = '/auth',
-    total = '/statics',
-    applicant = '/applicant',
-    applicants = '/applicants?size=10&page=0',
-    schedules = '/schedules',
-    ticket_print = '/excel/admission-ticket/',
-    applicants_print = '/excel/applicants',
-    make_applicants = '/applicants/examcode',
-    arrived_application = '/applicant/printed-arrived'
+    signin = '/admin/auth',
+    total = '/admin/statics',
+    applicant = '/admin/applicant',
+    applicants = '/admin/applicants?size=10&page=0',
+    schedules = '/admin/schedules',
+    ticket_print = '/admin/excel/admission-ticket/',
+    applicants_print = '/admin/excel/applicants',
+    make_applicants = '/admin/applicants/examcode',
+    arrived_application = '/admin/applicant/printed-arrived'
 }
 
 export default uri
