aacd9659ea9a301d0d7c5d2ba625dda5d16b0cd4
Hyeonyee <jgh10070@naver.com>
2021-07-02 22:34:52 +0900

    [FIX] status api 주석처리

--- src/data/api/status/index.ts
+++ src/data/api/status/index.ts
@@ -1,10 +1,10 @@
-import uri from '../uri';
-import { statusRequest } from 'src/models/dto/request/statusRequest';
-import { statusResponse } from 'src/models/dto/response/statusResponse';
-import { getRequest } from '../index';
+// import uri from '../uri';
+// import { statusRequest } from 'src/models/dto/request/statusRequest';
+// import { statusResponse } from 'src/models/dto/response/statusResponse';
+// import { getRequest } from '../index';
 
-export const getStatus = async (statusRequest: statusRequest) => {
-  const request = getRequest();
-  const response = await request.get<statusResponse>(uri.schedules);
-  return response;
-};
+// export const getStatus = async (statusRequest: statusRequest) => {
+//   const request = getRequest();
+//   const response = await request.get<statusResponse>(uri.schedules);
+//   return response;
+// };
