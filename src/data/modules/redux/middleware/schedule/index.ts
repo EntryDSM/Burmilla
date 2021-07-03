71af794ff1d4aa53bb9cac61c9befff8f6db5982
Hyeonyee <jgh10070@naver.com>
2021-07-03 23:50:57 +0900

    [FIX] userSaga에 setschedule추가

--- src/data/modules/redux/middleware/schedule/index.ts
+++ src/data/modules/redux/middleware/schedule/index.ts
@@ -1,12 +1,14 @@
-import { takeLatest } from 'redux-saga/effects';
+import { takeLatest, debounce } from 'redux-saga/effects';
 import createRequestSaga from '../../../../../utils/saga/createRequestSaga';
-import { getScheduleApi } from '../../../../api/index';
-import { GET_STATUS } from '../../../../modules/redux/action/schedule/interface';
+import { getScheduleApi, updateScheduleApi } from '../../../../api/index';
+import { GET_STATUS, SET_SCHEDULE } from '../../../../modules/redux/action/schedule/interface';
 
 export const getScheduleSaga = createRequestSaga(GET_STATUS, getScheduleApi);
+export const updateScheduleSaga = createRequestSaga(SET_SCHEDULE, updateScheduleApi);
 
 function* userSaga() {
   yield takeLatest(GET_STATUS, getScheduleSaga);
+  yield debounce(3000, SET_SCHEDULE, updateScheduleSaga);
 }
 
 export default userSaga;
