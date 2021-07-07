c7e4422968f845ca3396892787e89efc5c79fb62
Hyeonyee <jgh10070@naver.com>
2021-07-07 10:18:50 +0900

    [FIX] reducer => rootReducer로 이름 변경

--- src/data/modules/store.ts
+++ src/data/modules/store.ts
@@ -2,16 +2,16 @@ import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import createSagaMiddleware from 'redux-saga';
 
-import reducer from './redux/reducer';
+import rootReducer from './redux/reducer';
 import sagas from './redux/middleware';
 
 // const composeEnhancers = (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const sagaMiddleWare = createSagaMiddleware();
 
-export type AppState = ReturnType<typeof reducer>;
+export type AppState = ReturnType<typeof rootReducer>;
 
-export const store = createStore(reducer, composeWithDevTools(applyMiddleware(sagaMiddleWare)));
+export const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(sagaMiddleWare)));
 
 sagaMiddleWare.run(sagas);
-export type ReducerType = ReturnType<typeof reducer>;
+export type ReducerType = ReturnType<typeof rootReducer>;
