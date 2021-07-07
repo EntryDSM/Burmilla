c30d9b0335c641b523e5e6734deef5ec46a931f1
Hyeonyee <jgh10070@naver.com>
2021-07-07 10:21:48 +0900

    [FIX] statistics middleware 수정

--- src/data/modules/redux/middleware/statistics/index.ts
+++ src/data/modules/redux/middleware/statistics/index.ts
@@ -1,23 +1,30 @@
-import { all, takeLatest } from 'redux-saga/effects';
-
-import { sagaEntity } from '../index';
-import {
-  getStatisticsType,
-  GetStatistics,
-  GET_STATISTICS,
-  GET_STATISTICS_ASYNC,
-} from '../../action/statistics';
+import { takeLatest } from 'redux-saga/effects';
+import createRequestSaga from '../../../../../utils/saga/createRequestSaga';
 import { getStatisticsApi } from '../../../../api/index';
-import { GetStatisticsPayload } from '../../../../api/apiTypes';
+import { GET_STATISTICS } from "../../action/statistics/interface";
+// import { sagaEntity } from '../index';
+import {
+  // getStatisticsType,
+  // GetStatistics,
+  // GET_STATISTICS,
+  // GET_STATISTICS_ASYNC,
+} from '../../action/statistics';
 
-function* getStatistics(action: GetStatistics) {
-  yield sagaEntity<getStatisticsType, GetStatisticsPayload>({
-    action,
-    api: getStatisticsApi,
-    type: GET_STATISTICS_ASYNC,
-  });
+// function* getStatistics(action: GetStatistics) {
+//   yield sagaEntity<getStatisticsType>({
+//     action,
+//     api: getStatisticsApi,
+//     type: GET_STATISTICS_ASYNC,
+//   });
+// }
+
+// export default function* applicantSaga() {
+//   yield (takeLatest(GET_STATISTICS, getStatisticsSaga));
+// }
+export const getStatisticsSaga = createRequestSaga(GET_STATISTICS, getStatisticsApi);
+
+function* userSaga() {
+  yield takeLatest(GET_STATISTICS, getStatisticsSaga);
 }
 
-export default function* applicantSaga() {
-  yield all([takeLatest(GET_STATISTICS, getStatistics)]);
-}
+export default userSaga;
