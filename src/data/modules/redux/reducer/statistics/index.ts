ff7e9246e2d18940defef98a44419216cb729e1d
Hyeonyee <jgh10070@naver.com>
2021-07-07 10:19:59 +0900

    [FIX] Istatistics로 명명 변경, statistics reducer 수정

--- src/data/modules/redux/reducer/statistics/index.ts
+++ src/data/modules/redux/reducer/statistics/index.ts
@@ -1,80 +1,85 @@
-import {
-    StatisticsActions,
-    GET_STATISTICS_ASYNC,
-    SET_SELECTED_REGION,
+  import {
+    statisticsActionType,
+    // GET_STATISTICS_ASYNC,
   } from '../../action/statistics';
   import { GetEachStatisticsResponse } from '../../../../api/apiTypes';
-  import { returnApiResponseData } from '..';
-  import { API_STATUS } from '../../../../api/index';
+  import { STATISTICS_SUCCESS, STATISTICS_FAILURE } from "../../action/statistics/interface";
+  // import { returnApiResponseData } from '..';
+  // import { API_STATUS } from '../../../../api/index';
   
-  export interface InitialState {
-    getStatisticsStatus: 0 | 200 | 401 | 403;
+  export interface IStatisticsState {
+    getStatisticsStatus: 0 | 200 | 400 | 401;
     statistics: GetEachStatisticsResponse;
   }
   
-  const initialState: InitialState = {
+  const initialState: IStatisticsState = {
     getStatisticsStatus: 0,
     statistics: {
-      meister_applicant: {
-        '81-90': 0,
-        '71-80': 0,
-        '61-70': 0,
-        '51-60': 0,
-        '41-50': 0,
-        '31-40': 0,
-        '21-30': 0,
-        '-20': 0,
-        applicant_count: 0,
-        competition_rate: 0,
-      },
-      social_applicant: {
-        '81-90': 0,
-        '71-80': 0,
-        '61-70': 0,
-        '51-60': 0,
-        '41-50': 0,
-        '31-40': 0,
-        '21-30': 0,
-        '-20': 0,
-        applicant_count: 0,
-        competition_rate: 0,
-      },
-      common_applicant: {
-        '141-150': 2,
-        '131-140': 5,
-        '121-130': 6,
-        '111-120': 12,
-        '101-110': 8,
-        '91-100': 21,
-        '81-90': 2,
-        '-80': 12,
-        applicant_count: 68,
-        competition_rate: 1.7,
-      },
       total_applicant_count: 0,
       total_competition_rate: 0,
+      common_score: {
+        '141-150': 0,
+        '131-140': 0,
+        '121-130': 0,
+        '111-120': 0,
+        '101-110': 0,
+        '91-100': 0,
+        '81-90': 0,
+        '-80': 0,
+        common_count: 0,    
+        common_competition_rate: 0,   
+      },
+      meister_score: {
+        '81-90': 0,
+        '71-80': 0,
+        '61-70': 0,
+        '51-60': 0,
+        '41-50': 0,
+        '31-40': 0,
+        '21-30': 0,
+        '-20': 0,
+        special_count: 0,
+        special_competition_rate: 0,
+      },
+      social_score: {
+        '81-90': 0,
+        '71-80': 0,
+        '61-70': 0,
+        '51-60': 0,
+        '41-50': 0,
+        '31-40': 0,
+        '21-30': 0,
+        '-20': 0,
+        special_count: 0,
+        special_competition_rate: 0,
+      },
     }
   };
 
-  const applicantReducer = (state = initialState, action: StatisticsActions) => {
+  const statisticsReducer = (state: IStatisticsState = initialState, action: statisticsActionType) => {
     switch (action.type) {
-      case GET_STATISTICS_ASYNC:
-        return returnApiResponseData<InitialState>({
-          state,
-          statusName: API_STATUS.getStatisticsStatus,
-          payload: action.payload,
-          dataKeyName: 'statistics',
-        });
-      case SET_SELECTED_REGION: {
+      // case GET_STATISTICS_ASYNC:
+      //   return returnApiResponseData<InitialState>({
+      //     state,
+      //     statusName: API_STATUS.getStatisticsStatus,
+      //     dataKeyName: 'statistics',
+      //   });
+      case STATISTICS_SUCCESS: {
         return {
           ...state,
-          selectedRegion: action.payload.selectedRegion,
+          statistics: action.payload
         };
       }
-      default:
+      case STATISTICS_FAILURE: {
+        return {
+          ...state,
+        }
+      }
+      default: {
         return state;
+      }
     }
   };
   
-  export default applicantReducer;
+  export default statisticsReducer;
   
