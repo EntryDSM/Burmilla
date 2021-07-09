3283ec2c6dde7d527e69d479ba96f6076e31ffd5
Hyeonyee <jgh10070@naver.com>
2021-07-09 15:02:13 +0900

    [FIX] statistics action

--- src/data/modules/redux/action/statistics/index.ts
+++ src/data/modules/redux/action/statistics/index.ts
@@ -1,41 +1,49 @@
-import {
-    ApiPayload,
-    GetStatisticsPayload,
-    GetStatisticsResponse,
-  } from '../../../../api/apiTypes';
+  // export const GET_STATISTICS = 'GET_STATISTICS';
+  // export const GET_STATISTICS_ASYNC = 'GET_STATISTICS_ASYNC';
   
-  export const SET_SELECTED_REGION = 'SET_SELECTED_REGION';
-  export const GET_STATISTICS = 'GET_STATISTICS';
-  export const GET_STATISTICS_ASYNC = 'GET_STATISTICS_ASYNC';
+  // export type getStatisticsType =
+  //   | typeof GET_STATISTICS
+  //   | typeof GET_STATISTICS_ASYNC;
+  // export interface GetStatistics {
+  //   type: getStatisticsType;
+  // }
+  // export const getStatisticsAction = (
+  // ): GetStatistics => ({
+  //   type: GET_STATISTICS,
+  // });
   
-  export type getStatisticsType =
-    | typeof GET_STATISTICS
-    | typeof GET_STATISTICS_ASYNC;
-  export type getStatisticsPayload = ApiPayload<GetStatisticsResponse> &
-    GetStatisticsPayload;
-  export interface GetStatistics {
-    type: getStatisticsType;
-    payload: getStatisticsPayload;
-  }
-  export const getStatisticsAction = (
-    payload: getStatisticsPayload,
-  ): GetStatistics => ({
-    type: GET_STATISTICS,
-    payload,
-  });
-  
-  export type setSelectedRegionType = typeof SET_SELECTED_REGION;
-  export interface setSelectedRegionPayload {
-    selectedRegion: string;
-  }
-  export interface SetSelectedRegion {
-    type: setSelectedRegionType;
-    payload: setSelectedRegionPayload;
-  }
-  export const setSelectedRegionAction = (payload: setSelectedRegionPayload) => ({
-    type: SET_SELECTED_REGION,
-    payload,
-  });
-  
-  export type StatisticsActions = GetStatistics | SetSelectedRegion;
-  
+  // export type StatisticsActions = GetStatistics; 
+import { 
+  GET_STATISTICS, 
+  STATISTICS_SUCCESS, 
+  STATISTICS_FAILURE 
+} from "./interface";
+import { 
+  CommonScoreDistribution, 
+  SpecialScoreDistribution 
+} from "../../../../api/apiTypes";
+
+export const getStatisticsSuccess = (payload: {
+  total_applicant_count: number;
+  total_competition_rate: number;
+  common_score: CommonScoreDistribution;
+  meister_score: SpecialScoreDistribution;
+  social_score: SpecialScoreDistribution;
+}) => ({
+  type: STATISTICS_SUCCESS,
+  payload,
+})
+
+export const getStatisticsFailure = (payload: number) => ({
+  type: STATISTICS_FAILURE,
+  payload,
+})
+
+export const getStatistics = () => ({
+  type: GET_STATISTICS,
+})
+
+export type statisticsActionType = 
+  | ReturnType<typeof getStatisticsSuccess>
+  | ReturnType<typeof getStatisticsFailure>
+  | ReturnType<typeof getStatistics>;
