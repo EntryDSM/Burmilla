8a944995992e779bb387ddf0a39417a6083a7278
Hyeonyee <jgh10070@naver.com>
2021-07-10 23:36:31 +0900

    [FIX] useSchedule

--- src/hooks/schedule/useSchedule.ts
+++ src/hooks/schedule/useSchedule.ts
@@ -6,11 +6,24 @@ import {
   setIsStart,
   setStatus,
   getStatus,
-  getStatusSuccess,
-  // UpdateSchedulesPayload,
-  setSchedule
+  setStartScheduleMonth,
+  setStartScheduleDay,
+  setEndScheduleMonth,
+  setEndScheduleDay,
+  setFirstScheduleMonth,
+  setFirstScheduleDay,
+  setInterviewScheduleMonth,
+  setInterviewScheduleDay,
+  setSecondScheduleMonth,
+  setSecondScheduleDay,
+  setStartDate,
+  setEndDate,
+  setFirstDate,
+  setInterviewDate,
+  setSecondDate,
+  setSchedule,
 } from '../../data/modules/redux/action/schedule';
-import { processTimeType, processType } from '../../data/modules/redux/reducer/schedule/interface';
+import { scheduleUpdateTypes, processType } from '../../data/modules/redux/reducer/schedule/interface';
 import { scheduleType } from '../../data/modules/redux/reducer/schedule/scheduleConstance';
 
 const useSchedule = () => {
@@ -22,9 +35,54 @@ const useSchedule = () => {
     setIsEnd: (payload: boolean) => dispatch(setIsEnd(payload)),
     setProcess: (payload: processType[]) => dispatch(setProcess(payload)),
     getStatus: () => dispatch(getStatus()),
-    getStatusSuccess: (payload: { schedules: Array<processTimeType>; current_status: string }) =>
-      dispatch(getStatusSuccess(payload)),
-    setSchedule: (payload: { schedules: Array<processTimeType> }) => {
+    setStartScheduleMonth: (payload: number) => { 
+      dispatch(setStartScheduleMonth(payload));
+    },
+    setStartScheduleDay: (payload: number) => { 
+      dispatch(setStartScheduleDay(payload));
+    },
+    setEndScheduleMonth: (payload: number) => {
+      dispatch(setEndScheduleMonth(payload));
+    },
+    setEndScheduleDay: (payload: number) => {
+      dispatch(setEndScheduleDay(payload));
+    },
+    setFirstScheduleMonth: (payload: number) => {
+      dispatch(setFirstScheduleMonth(payload));
+    },
+    setFirstScheduleDay: (payload: number) => {
+      dispatch(setFirstScheduleDay(payload));
+    },
+    setInterviewScheduleMonth: (payload: number) => {
+      dispatch(setInterviewScheduleMonth(payload));
+    },
+    setInterviewScheduleDay: (payload: number) => {
+      dispatch(setInterviewScheduleDay(payload));
+    },
+    setSecondScheduleMonth: (payload: number) => {
+      dispatch(setSecondScheduleMonth(payload));
+    },
+    setSecondScheduleDay: (payload: number) => {
+      dispatch(setSecondScheduleDay(payload));
+    },
+    setStartDate: (payload: string) => {
+      dispatch(setStartDate(payload))
+    },
+    setEndDate: (payload: string) => {
+      dispatch(setEndDate(payload));
+    },
+    setFirstDate: (payload: string) => {
+      dispatch(setFirstDate(payload))
+    },
+    setInterviewDate: (payload: string) => {
+      dispatch(setInterviewDate(payload))
+    },
+    setSecondDate: (payload: string) => {
+      dispatch(setSecondDate(payload))
+    },
+    setSchedule: (payload: { 
+      schedules: Array<scheduleUpdateTypes>; 
+    }) => {
       dispatch(setSchedule(payload))
     },
   };
