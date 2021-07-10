d554f9f3e04877a3ebae313b9a3e547b25d51869
Hyeonyee <jgh10070@naver.com>
2021-07-10 23:52:50 +0900

    [FIX] scheduleContainer

--- src/containers/schedule/ScheduleContainer.tsx
+++ src/containers/schedule/ScheduleContainer.tsx
@@ -1,15 +1,27 @@
 import React, { FC, Suspense } from "react";
 import { useSchedule } from "../../hooks/schedule";
+import { useAuth } from "../../hooks/auth";
+import { getRefreshToken } from "../../utils/token";
 
 const Schedule = React.lazy(() => import("../../components/Schedule"));
 
 const ScheduleContainer: FC = () => {
-  const scheduleState = useSchedule();
+  const { state, setState } = useSchedule();
+  const { refreshtoken } = useAuth();
 
-  const dates = scheduleState.state.date;
+  React.useEffect(() => {
+    const errorStatus = state.error.status;
+    if (errorStatus === 401 || errorStatus === 403) {
+      refreshtoken({
+        refreshToken: getRefreshToken(),
+      });
+      console.log(refreshtoken);
+    }
+  }, [state.error]);
+
   return (
     <Suspense fallback={<div>로딩중...</div>}>
-      <Schedule />
+      <Schedule {...state} {...setState} />;
     </Suspense>
   );
 };
