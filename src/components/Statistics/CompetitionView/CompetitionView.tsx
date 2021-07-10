908fcc75f38d3a2120cf9a76a1198137b2700cb5
Hyeonyee <jgh10070@naver.com>
2021-07-10 23:53:17 +0900

    [FIX] competitionView

--- src/components/Statistics/CompetitionView/CompetitionView.tsx
+++ src/components/Statistics/CompetitionView/CompetitionView.tsx
@@ -1,32 +1,28 @@
 import React, { FC } from "react";
 import * as S from "../style";
-import { useStatistics } from "../../../hooks/statistics";
 
-const CompetitionView: FC = () => {
-  const {
-    statisticsStore: {
-      statistics: { total_applicant_count, total_competition_rate },
-    },
-    getStatistics,
-  } = useStatistics();
+interface Props {
+  totalApplicantCount: number;
+  totalCompetitionRate: number;
+}
 
-  React.useEffect(() => {
-    // getStatistics({ area: "all" });
-  }, []);
-
+const CompetitionView: FC<Props> = ({
+  totalApplicantCount,
+  totalCompetitionRate,
+}) => {
   return (
     <S.CompetitionViewWrapper className="no-select">
       <tbody>
         <tr>
           <S.CompetitionViewTH>지원자 수 : </S.CompetitionViewTH>
           <S.CompetitionViewTD>
-            <b>{total_applicant_count}</b> 명
+            <b>{totalApplicantCount}</b> 명
           </S.CompetitionViewTD>
         </tr>
         <tr>
           <S.CompetitionViewTH>경쟁률 : </S.CompetitionViewTH>
           <S.CompetitionViewTD>
-            <b>{total_competition_rate} : 1</b>
+            <b>{totalCompetitionRate} : 1</b>
           </S.CompetitionViewTD>
         </tr>
       </tbody>
