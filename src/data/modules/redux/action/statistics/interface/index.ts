77148acc4550bf282c609e64a5c11107c5cd5991
Hyeonyee <jgh10070@naver.com>
2021-07-10 23:51:55 +0900

    [FIX]statistics action

--- src/data/modules/redux/action/statistics/interface/index.ts
+++ src/data/modules/redux/action/statistics/interface/index.ts
@@ -1,3 +1,6 @@
-export const GET_STATISTICS = 'GET_STATISTICS' as const;
-export const STATISTICS_SUCCESS = 'STATISTICS_SUCCESS' as const;
-export const STATISTICS_FAILURE = 'STATISTICS_FAILURE' as const;
+// export const GET_STATISTICS = 'GET_STATISTICS' as const;
+// export const GET_STATISTICS_ASYNC = 'GET_STATISTICS_ASYNC';
+
+export const GET_STATISTICS = 'statistics/GET_STATISTICS' as const;
+export const GET_STATISTICS_SUCCESS = 'statistics/GET_STATISTICS_SUCCESS' as const;
+export const GET_STATISTICS_FAILURE = 'statistics/GET_STATISTICS_FAILURE' as const;
