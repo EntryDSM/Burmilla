9df83edba8bc96cf922e4682fcc6cc92a9f40aca
Hyeonyee <jgh10070@naver.com>
2021-07-10 23:51:20 +0900

    [FIX] statistics middleware

--- src/data/modules/redux/middleware/statistics/index.ts
+++ src/data/modules/redux/middleware/statistics/index.ts
@@ -2,29 +2,11 @@ import { takeLatest } from 'redux-saga/effects';
 import createRequestSaga from '../../../../../utils/saga/createRequestSaga';
 import { getStatisticsApi } from '../../../../api/index';
 import { GET_STATISTICS } from "../../action/statistics/interface";
-// import { sagaEntity } from '../index';
-import {
-  // getStatisticsType,
-  // GetStatistics,
-  // GET_STATISTICS,
-  // GET_STATISTICS_ASYNC,
-} from '../../action/statistics';
 
-// function* getStatistics(action: GetStatistics) {
-//   yield sagaEntity<getStatisticsType>({
-//     action,
-//     api: getStatisticsApi,
-//     type: GET_STATISTICS_ASYNC,
-//   });
-// }
-
-// export default function* applicantSaga() {
-//   yield (takeLatest(GET_STATISTICS, getStatisticsSaga));
-// }
 export const getStatisticsSaga = createRequestSaga(GET_STATISTICS, getStatisticsApi);
 
-function* userSaga() {
+function* statisticsSaga() {
   yield takeLatest(GET_STATISTICS, getStatisticsSaga);
 }
 
-export default userSaga;
+export default statisticsSaga;
