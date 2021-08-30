595d9d436de471c4c145b0b22fa31b944f1a84dc
Hyeonyee <jgh10070@naver.com>
2021-08-31 00:04:23 +0900

    [FIX] import path

--- src/hooks/default/useHeader.tsx
+++ src/hooks/default/useHeader.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Header from "../../components/Header/Hedaer";
+import Header from "../../components/header/Header";
 
 const useHeader = () => {
   return <Header />;
