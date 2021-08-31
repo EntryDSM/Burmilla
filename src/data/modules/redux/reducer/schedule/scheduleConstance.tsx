afc5ea19c401c5f71fd5e0382a2dff38b07e146f
Hyeonyee <jgh10070@naver.com>
2021-08-31 22:35:08 +0900

    [FIX] scheduleConstance

--- src/data/modules/redux/reducer/schedule/scheduleConstance.tsx
+++ src/data/modules/redux/reducer/schedule/scheduleConstance.tsx
@@ -7,9 +7,11 @@ export const FIRST_ANNOUNCEMENT = "FIRST_ANNOUNCEMENT" as const;
 export const BEFORE_INTERVIEW = "BEFORE_INTERVIEW" as const;
 export const INTERVIEW = "INTERVIEW" as const;
 export const SECOND_ANNOUNCEMENT = "SECOND_ANNOUNCEMENT" as const;
-export const BEFORE_SECOND_ANNOUNCEMENT = "BEFORE_SECOND_ANNOUNCEMENT" as const;
+export const BEFORE_SECOND_ANNOUNCE = "BEFORE_SECOND_ANNOUNCE" as const;
 export const NOT_APPLICATION_PERIOD = "NOT_APPLICATION_PERIOD" as const;
-export const BEFORE_FIRST_ANNOUNCEMENT = "BEFORE_FIRST_ANNOUNCEMENT" as const;
+export const BEFORE_FIRST_ANNOUNCE = "BEFORE_FIRST_ANNOUNCE" as const;
+export const APPLICATION_PERIOD = "APPLICATION_PERIOD" as const;
+export const END = "END" as const;
 
 export type scheduleType =
   | typeof START_DATE
@@ -18,9 +20,11 @@ export type scheduleType =
   | typeof SECOND_ANNOUNCEMENT
   | typeof INTERVIEW
   | typeof NOT_APPLICATION_PERIOD
-  | typeof BEFORE_FIRST_ANNOUNCEMENT
-  | typeof BEFORE_SECOND_ANNOUNCEMENT
-  | typeof BEFORE_INTERVIEW;
+  | typeof BEFORE_FIRST_ANNOUNCE
+  | typeof BEFORE_SECOND_ANNOUNCE
+  | typeof BEFORE_INTERVIEW
+  | typeof END
+  | typeof APPLICATION_PERIOD;
 
 const scheduleConstance: Record<scheduleType, processType> = {
   [NOT_APPLICATION_PERIOD]: {
@@ -37,7 +41,7 @@ const scheduleConstance: Record<scheduleType, processType> = {
     ),
     buttonText: "일정 수정",
   },
-  [BEFORE_FIRST_ANNOUNCEMENT]: {
+  [BEFORE_FIRST_ANNOUNCE]: {
     title: <p>원서 접수가 끝났습니다.</p>,
     getDescription: (date?: string) => (
       <p>1차 발표일은 {<span>{date}</span>} 입니다.</p>
@@ -63,7 +67,7 @@ const scheduleConstance: Record<scheduleType, processType> = {
     ),
     buttonText: "일정 수정",
   },
-  [BEFORE_SECOND_ANNOUNCEMENT]: {
+  [BEFORE_SECOND_ANNOUNCE]: {
     title: <p>면접이 끝났습니다.</p>,
     getDescription: (date?: string) => (
       <p>2차 발표일은 {<span>{date}</span>} 입니다.</p>
@@ -78,20 +82,31 @@ const scheduleConstance: Record<scheduleType, processType> = {
     buttonText: "일정 수정",
   },
   [END_DATE]: {
-    title: "원서 접수가 끝났습니다.",
+    title: "원서 접수가 마감되었습니다.",
+    getDescription: () => "",
+    buttonText: "일정 수정",
+  },
+  [END]: {
+    title: "2022년 입학 전형이 끝났습니다.",
     getDescription: () => <p>내년을 기약해 주세요.</p>,
+    buttonText: "일정 수정",
+  },
+  [APPLICATION_PERIOD]: {
+    title: <p>지금은 {<span>원서 작성</span>} 기간입니다.</p>,
+    getDescription: (date: string) => (
+      <p>원서 접수 기간은 {<span>{date}</span>}까지 입니다.</p>
+    ),
     buttonText: "일정 수정",
   },
 };
 
 export const mainProcessNumber = {
   [NOT_APPLICATION_PERIOD]: 1,
-  [START_DATE]: 2,
-  [BEFORE_FIRST_ANNOUNCEMENT]: 3,
+  [APPLICATION_PERIOD]: 2,
+  [BEFORE_FIRST_ANNOUNCE]: 3,
   [FIRST_ANNOUNCEMENT]: 4,
   [INTERVIEW]: 5,
   [SECOND_ANNOUNCEMENT]: 6,
-  [END_DATE]: 7,
 };
 
 export default scheduleConstance;
