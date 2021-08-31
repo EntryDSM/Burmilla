12d1a92e3661fcce6f60ae03fc2c171d661cdb3e
Hyeonyee <jgh10070@naver.com>
2021-08-31 22:39:07 +0900

    [FIX] GET_STATISTICS GET_STATISTICS_ASYNC 삭제

--- src/data/modules/redux/action/statistics/interface/index.ts
+++ src/data/modules/redux/action/statistics/interface/index.ts
@@ -1,6 +1,3 @@
-// export const GET_STATISTICS = 'GET_STATISTICS' as const;
-// export const GET_STATISTICS_ASYNC = 'GET_STATISTICS_ASYNC';
-
 export const GET_STATISTICS = 'statistics/GET_STATISTICS' as const;
 export const GET_STATISTICS_SUCCESS = 'statistics/GET_STATISTICS_SUCCESS' as const;
 export const GET_STATISTICS_FAILURE = 'statistics/GET_STATISTICS_FAILURE' as const;
