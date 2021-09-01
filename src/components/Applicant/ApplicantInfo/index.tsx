b8867b563ab607b58c5cb03b60b10a685d55ca0f
Hyeonyee <jgh10070@naver.com>
2021-09-01 17:07:16 +0900

    [FIX] ApplicantInfo 전부 추가

--- src/components/Applicant/ApplicantInfo/index.tsx
+++ src/components/Applicant/ApplicantInfo/index.tsx
@@ -1,31 +1,48 @@
 import React, { FC } from "react";
+import Unsubmitted from "./unSubmitted";
+import Submitted from "./submitted";
+import {
+  GetApplicantsListResponse,
+  GetApplicantInfoResponse,
+  UpdateApplicantStatusPayload,
+} from "../../../data/api/apiTypes";
 
-import Unsubmitted from "./Unsubmitted";
-import Submitted from "./Submitted";
-import { useApplicant } from "../../../hooks/applicant";
+interface Props {
+  applicantsList: GetApplicantsListResponse;
+  currnetApplicantInfo: GetApplicantInfoResponse;
+  updateApplicantStatusStatus: number;
+  updateApplicantStatus: UpdateApplicantStatusPayload;
+  resetUpdateStatus;
+}
 
-const ApplicantInfo: FC = () => {
-  const {
-    applicantStore: {
-      currnetApplicantInfo: {
-        applicant_information,
-        applicant_contact,
-        school_tel,
-      },
-    },
-  } = useApplicant();
-
-  return applicant_information ? (
-    <Submitted
-      applicantPrivacy={applicant_information.privacy}
-      applicantEvaluation={applicant_information.evaluation}
-      applicantStatus={applicant_information.status}
-    />
+const ApplicantInfo: FC<Props> = ({
+  applicantsList,
+  currnetApplicantInfo,
+  updateApplicantStatusStatus,
+  updateApplicantStatus,
+  resetUpdateStatus,
+}) => {
+  return currnetApplicantInfo.applicant_information ? (
+    <>
+      {applicantsList.applicants_information_response &&
+        applicantsList.applicants_information_response.map((info) => (
+          <Submitted
+            applicantPersonalData={
+              currnetApplicantInfo.applicant_information.personal_data
+            }
+            applicantEvaluation={
+              currnetApplicantInfo.applicant_information.evaluation
+            }
+            applicantStatus={currnetApplicantInfo.applicant_information.status}
+            applicantListItem={info}
+            updateApplicantStatusStatus={updateApplicantStatusStatus}
+            updateApplicantStatus={updateApplicantStatus}
+            resetUpdateStatus={resetUpdateStatus}
+          />
+        ))}
+    </>
   ) : (
-    <Unsubmitted
-      applicant_contact={applicant_contact}
-      school_tel={school_tel || null}
-    />
+    <Unsubmitted applicant_contact={currnetApplicantInfo.applicant_contact} />
   );
 };
 
