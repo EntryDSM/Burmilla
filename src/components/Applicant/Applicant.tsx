6e6b5fb91edf7dc2c7cacce6222a37c11d0ded15
Hyeonyee <jgh10070@naver.com>
2021-09-01 17:06:14 +0900

    [FIX] Applicant

--- src/components/Applicant/Applicant.tsx
+++ src/components/Applicant/Applicant.tsx
@@ -1,30 +1,54 @@
 import React, { FC } from "react";
 import * as S from "./style";
-import ApplicantInfo from "./ApplicantInfo";
-import ApplicantsList from "./ApplicantsList";
-import FilterSearch from "./FilterSearch";
-import Pagination from "./Pagination";
-import { useApplicant } from "../../hooks/applicant";
+import ApplicantInfo from "./applicantInfo";
+import ApplicantsList from "./applicantsList";
+import FilterSearch from "./filterSearch";
+import Pagination from "./pagination";
+import {
+  GetApplicantsListResponse,
+  GetApplicantsListPayload,
+  GetApplicantInfoResponse,
+  UpdateApplicantStatusPayload,
+} from "../../data/api/apiTypes";
 
-const Applicant: FC = () => {
-  const {
-    applicantStore: { currnetApplicantInfo, filters },
-    getApplicantsList,
-  } = useApplicant();
+interface Props {
+  applicantsList: GetApplicantsListResponse;
+  filters: GetApplicantsListPayload;
+  currnetApplicantInfo: GetApplicantInfoResponse;
+  updateApplicantStatusStatus: number;
+  updateApplicantStatus: UpdateApplicantStatusPayload;
+  // updateApplicantList: ;
+  resetUpdateStatus;
+  // getApplicantInfo: (payload: ) => void;
+  setFilter: (payload: GetApplicantsListPayload) => void;
+}
 
-  React.useEffect(() => {
-    // getApplicantsList(filters);
-  }, [filters]);
-
+const Applicant: FC<Props> = ({
+  applicantsList,
+  filters,
+  currnetApplicantInfo,
+  updateApplicantStatusStatus,
+  updateApplicantStatus,
+  resetUpdateStatus,
+  setFilter,
+}) => {
   return (
     <S.Applicant>
       <S.ApplicantContainer>
-        <FilterSearch />
-        <ApplicantsList />
-        <Pagination />
+        <FilterSearch filters={filters} setFilter={setFilter} />
+        <ApplicantsList applicantsList={applicantsList} filters={filters} />
+        <Pagination applicantsList={applicantsList} setFilter={setFilter} />
       </S.ApplicantContainer>
       <S.ApplicantInfoWrap>
-        {currnetApplicantInfo && <ApplicantInfo />}
+        {currnetApplicantInfo && (
+          <ApplicantInfo
+            applicantsList={applicantsList}
+            currnetApplicantInfo={currnetApplicantInfo}
+            updateApplicantStatusStatus={updateApplicantStatusStatus}
+            updateApplicantStatus={updateApplicantStatus}
+            resetUpdateStatus={resetUpdateStatus}
+          />
+        )}
       </S.ApplicantInfoWrap>
     </S.Applicant>
   );
