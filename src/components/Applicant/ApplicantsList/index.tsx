800a679f20aba4b94487831ad0397a1f193933a7
Hyeonyee <jgh10070@naver.com>
2021-09-01 17:14:45 +0900

    [FIX] applicantsList 추가

--- src/components/Applicant/ApplicantsList/index.tsx
+++ src/components/Applicant/ApplicantsList/index.tsx
@@ -1,31 +1,36 @@
 import React, { FC } from "react";
 
 import * as S from "./style";
-import ApplicantsListHeader from "./ApplicantsListHeader";
-import ApplicantsListItem from "./ApplicantsListItem";
-import { useApplicant } from "../../../hooks/applicant";
+import ApplicantsListHeader from "./applicantsListHeader";
+import ApplicantsListItem from "./applicantsListItem";
+import {
+  GetApplicantsListPayload,
+  GetApplicantsListResponse,
+} from "../../../data/api/apiTypes";
 
-const ApplicantsList: FC = () => {
-  const {
-    applicantStore: { applicantsList },
-    getApplicantInfo,
-  } = useApplicant();
+interface Props {
+  filters: GetApplicantsListPayload;
+  applicantsList: GetApplicantsListResponse;
+}
 
-  const handleClickListItem = React.useCallback((email: string) => {
-    getApplicantInfo({ email });
+const ApplicantsList: FC<Props> = ({ applicantsList, filters }) => {
+  const handleClickListItem = React.useCallback((receipt_code: number) => {
+    // getApplicantInfo({ receipt_code });
   }, []);
 
   return (
     <S.ApplicantsList>
       <ApplicantsListHeader />
       <tbody className="no-select">
-        {applicantsList.applicants_information.map((info) => (
-          <ApplicantsListItem
-            key={info.email}
-            applicantInfo={info}
-            handleClick={handleClickListItem}
-          />
-        ))}
+        {applicantsList.applicants_information_response &&
+          applicantsList.applicants_information_response.map((info) => (
+            <ApplicantsListItem
+              filters={filters}
+              key={info.receipt_code}
+              applicantInfo={info}
+              handleClick={handleClickListItem}
+            />
+          ))}
       </tbody>
     </S.ApplicantsList>
   );
