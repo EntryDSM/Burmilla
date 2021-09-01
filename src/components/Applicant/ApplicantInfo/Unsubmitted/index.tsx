b1ba525785b87b26c1b12208a811b32922f69f03
Hyeonyee <jgh10070@naver.com>
2021-09-01 17:15:06 +0900

    [FIX] Unsubmitted

--- src/components/Applicant/ApplicantInfo/Unsubmitted/index.tsx
+++ src/components/Applicant/ApplicantInfo/Unsubmitted/index.tsx
@@ -1,33 +1,37 @@
 import React, { FC } from "react";
-
+import { JsxElement } from "typescript";
 import * as S from "./style";
 
 interface Props {
   applicant_contact: {
     email: string;
-    applicant_tel: number;
+    telephone_number: number;
     parent_tel: number;
+    school_tel?: number;
     home_tel: number;
   };
-  school_tel?: number;
 }
 
 const Unsubmitted: FC<Props> = ({
-  applicant_contact: { email, applicant_tel, parent_tel, home_tel },
-  school_tel,
+  applicant_contact: {
+    email,
+    telephone_number,
+    parent_tel,
+    home_tel,
+    school_tel,
+  },
 }) => {
-  return (
-    <S.Wrapper>
-      <h3>최종제출을 하지 않은 지원자입니다.</h3>
-      <table>
-        <tbody>
+  const checkIsSubmitted = () => {
+    if (school_tel !== null) {
+      return (
+        <>
           <S.BasicInfoTableTR>
             <th>이메일</th>
             <td>{email}</td>
           </S.BasicInfoTableTR>
           <S.BasicInfoTableTR>
             <th>학생 연락처</th>
-            <td>{applicant_tel}</td>
+            <td>{telephone_number}</td>
           </S.BasicInfoTableTR>
           <S.BasicInfoTableTR>
             <th>부모님 연락처</th>
@@ -39,9 +43,40 @@ const Unsubmitted: FC<Props> = ({
           </S.BasicInfoTableTR>
           <S.BasicInfoTableTR>
             <th>자택 연락처</th>
-            <td>{school_tel}</td>
+            <td>{home_tel}</td>
           </S.BasicInfoTableTR>
-        </tbody>
+        </>
+      );
+    }
+    if (school_tel === null) {
+      return (
+        <>
+          <S.BasicInfoTableTR>
+            <th>이메일</th>
+            <td>{email}</td>
+          </S.BasicInfoTableTR>
+          <S.BasicInfoTableTR>
+            <th>학생 연락처</th>
+            <td>{telephone_number}</td>
+          </S.BasicInfoTableTR>
+          <S.BasicInfoTableTR>
+            <th>부모님 연락처</th>
+            <td>{parent_tel}</td>
+          </S.BasicInfoTableTR>
+          <S.BasicInfoTableTR>
+            <th>자택 연락처</th>
+            <td>{home_tel}</td>
+          </S.BasicInfoTableTR>
+        </>
+      );
+    }
+  };
+
+  return (
+    <S.Wrapper>
+      <h3>최종제출을 하지 않은 지원자입니다.</h3>
+      <table>
+        <tbody>{checkIsSubmitted}</tbody>
       </table>
     </S.Wrapper>
   );
