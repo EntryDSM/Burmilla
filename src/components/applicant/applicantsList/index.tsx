99ef71f18867dbbba7c6d0f8d9a4b5dc8476003d
Hyeonyee <jgh10070@naver.com>
2021-09-03 19:28:25 +0900

    [FIX]  currentApplicantInfo props로 추가

--- src/components/applicant/applicantsList/index.tsx
+++ src/components/applicant/applicantsList/index.tsx
@@ -1,23 +1,25 @@
 import React, { FC } from "react";
-
 import * as S from "./style";
 import ApplicantsListHeader from "./applicantsListHeader";
 import ApplicantsListItem from "./applicantsListItem";
 import {
   GetApplicantsListPayload,
   GetApplicantsListResponse,
+  GetApplicantInfoResponse,
   GetApplicantInfoPayload,
 } from "../../../data/api/apiTypes";
 
 interface Props {
   filters: GetApplicantsListPayload;
+  currnetApplicantInfo: GetApplicantInfoResponse;
   applicantsList: GetApplicantsListResponse;
   getApplicantInfo: (payload: GetApplicantInfoPayload) => void;
 }
 
 const ApplicantsList: FC<Props> = ({
-  applicantsList,
   filters,
+  currnetApplicantInfo,
+  applicantsList,
   getApplicantInfo,
 }) => {
   const handleClickListItem = React.useCallback((receipt_code: number) => {
@@ -28,10 +30,11 @@ const ApplicantsList: FC<Props> = ({
     <S.ApplicantsList>
       <ApplicantsListHeader />
       <tbody className="no-select">
-        {applicantsList.applicants_information_response &&
-          applicantsList.applicants_information_response.map((info) => (
+        {applicantsList.applicants_information_responses &&
+          applicantsList.applicants_information_responses.map((info) => (
             <ApplicantsListItem
               filters={filters}
+              currnetApplicantInfo={currnetApplicantInfo}
               key={info.receipt_code}
               applicantInfo={info}
               handleClick={handleClickListItem}
