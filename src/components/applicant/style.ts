d2eeab6030416445fe7e509665a23c11da4d542e
Hyeonyee <jgh10070@naver.com>
2021-09-03 19:26:13 +0900

    [FIX] pagination 감싸는 태그

--- src/components/applicant/style.ts
+++ src/components/applicant/style.ts
@@ -16,7 +16,14 @@ export const ApplicantContainer = styled.div`
   flex: 4;
   display: flex;
   flex-direction: column;
-  position: relative;
+`;
+
+export const PaginationBox = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: flex-end;
+  width: 100%;
+  height: 8%;
 `;
 
 export const ApplicantInfoWrap = styled.div`
