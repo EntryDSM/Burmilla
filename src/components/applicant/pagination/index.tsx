3d74181be942c80d1f7e0c16bb066bb877b689fe
Hyeonyee <jgh10070@naver.com>
2021-09-06 23:57:17 +0900

    [FIX] pagination

--- src/components/applicant/pagination/index.tsx
+++ src/components/applicant/pagination/index.tsx
@@ -1,5 +1,4 @@
 import React, { FC } from "react";
-
 import * as S from "./style";
 import { shift_left_icon, shift_right_icon } from "../../../assets/applicants";
 import { getIndexList } from "../../../utils/pagination";
@@ -10,39 +9,34 @@ import {
 
 interface Props {
   applicantsList: GetApplicantsListResponse;
+  filters: GetApplicantsListPayload;
   setFilter: (payload: GetApplicantsListPayload) => void;
 }
 
-const Pagination: FC<Props> = ({ applicantsList, setFilter }) => {
+const Pagination: FC<Props> = ({ applicantsList, filters, setFilter }) => {
   const [currentIndex, setCurrentIndex] = React.useState(1);
   const [indexList, setIndexList] = React.useState<number[]>([]);
 
   React.useEffect(() => {
     setIndexList(getIndexList(currentIndex, applicantsList.total_pages));
-  }, [applicantsList.total_pages, currentIndex]);
+  }, [currentIndex, applicantsList.total_pages]);
 
   React.useEffect(() => {
-    setCurrentIndex(1);
-    setFilter({
-      page: currentIndex,
-    });
+    setCurrentIndex(0);
   }, [applicantsList.total_pages]);
 
-  const handleClickIndex = React.useCallback(
-    (index: number) => {
-      setCurrentIndex(index);
-      setFilter({
-        page: currentIndex,
-      });
-    },
-    [currentIndex, applicantsList.total_pages]
-  );
+  const handleClickIndex = React.useCallback((index: number) => {
+    setCurrentIndex(index);
+    setFilter({
+      page: index - 1,
+    });
+  }, []);
   const handleClickPrev = React.useCallback(() => {
     if (currentIndex > 1) setCurrentIndex(currentIndex - 1);
     setFilter({
       page: currentIndex,
     });
-  }, [currentIndex, applicantsList.total_pages]);
+  }, [currentIndex]);
   const handleClickNext = React.useCallback(() => {
     if (currentIndex < applicantsList.total_pages)
       setCurrentIndex(currentIndex + 1);
