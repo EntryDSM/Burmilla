6107930f1880088c85ff1d0f01d4073ea69e7317
Hyeonyee <jgh10070@naver.com>
2021-09-06 23:56:19 +0900

    [FIX] applicant reducer

--- src/data/modules/redux/reducer/applicant/index.ts
+++ src/data/modules/redux/reducer/applicant/index.ts
@@ -2,6 +2,7 @@ import { ApplicantState } from "./interface";
 import { applicantActionType } from '../../action/applicant';
 import { 
   SET_FILTER,
+  SET_APPLICANT_INFO_APPEAR,
   GET_APPLICANT_INFO_SUCCESS,
   GET_APPLICANT_INFO_FAILURE,
   GET_APPLICANTS_LIST_SUCCESS,
@@ -22,6 +23,7 @@ const InitialState: ApplicantState = {
   getApplicantsListStatus: 0,
   getApplicantInfoStatus: 0,
   updateApplicantStatusStatus: 0,
+  applicantInfoAppear: false,
   filters: {
     size: 10,
     page: 0,
@@ -40,48 +42,48 @@ const InitialState: ApplicantState = {
     total_pages: 0,
     applicants_information_responses: [],
   },
-  currnetApplicantInfo: null,
-  // currnetApplicantInfo: {
-  //   submitted_applicant: {
-  //     status: {
-  //       is_printed_arrived: false,
-  //       is_submit: false,
-  //     },
-  //     personal_data: {
-  //       photo_file_name: '',
-  //       name: '',
-  //       birth_date: '',
-  //       school_name: '',
-  //       is_graduated: false,
-  //       educational_status: '',
-  //       application_type: '',
-  //       address: '',
-  //       detail_address: '',
-  //       email: '',
-  //       telephone_number: '',
-  //       parent_tel: '',
-  //       school_tel: '',
-  //       home_tel: '',
-  //     },
-  //     evaluation: {
-  //       volunteer_time: 0,
-  //       conversion_score: 0,
-  //       day_absence_count: 0,
-  //       lecture_absence_count: 0,
-  //       early_leave_count: 0,
-  //       lateness_count: 0,
-  //       self_introduce: '',
-  //       study_plan: '',
-  //     },
-  //   },
-  //   not_submitted_applicant: {
-  //     email: '',
-  //     telephone_number: 0,
-  //     parent_tel: 0,
-  //     home_tel: 0,
-  //     school_tel: 0,
-  //   }
-  // },
+  currnetApplicantInfo: {
+    submitted_applicant: {
+      status: {
+        printed_arrived: false,
+        submit: false,
+      },
+      personal_data: {
+        photo_file_name: '',
+        name: '',
+        email: '',
+        birth_date: '',
+        school_name: '',
+        educational_status: '',
+        application_type: '',
+        address: '',
+        detail_address: '',
+        telephone_number: '',
+        parent_tel: '',
+        school_tel: '',
+        home_tel: '',
+        graduated: false,
+      },
+      evaluation: {
+        volunteer_time: 0,
+        conversion_score: 0,
+        day_absence_count: 0,
+        lecture_absence_count: 0,
+        early_leave_count: 0,
+        lateness_count: 0,
+        self_introduce: '',
+        study_plan: '',
+        average_score: 0,
+      },
+    },
+    not_submitted_applicant: {
+      email: '',
+      applicant_tel: '',
+      parent_tel: '',
+      home_tel: '',
+      school_tel: '',
+    }
+  },
   error: {
     status: 0,
     message: '',
@@ -91,6 +93,12 @@ const InitialState: ApplicantState = {
 
 const applicantReducer = (state: ApplicantState = InitialState, action: applicantActionType) => {
   switch (action.type) {
+    case SET_APPLICANT_INFO_APPEAR: {
+      return {
+        ...state,
+        applicantInfoAppear: action.payload,
+      };
+    }
     case SET_FILTER: {
       return {
         ...state,
@@ -119,54 +127,28 @@ const applicantReducer = (state: ApplicantState = InitialState, action: applican
     case GET_APPLICANT_INFO_SUCCESS: {
       return {
         ...state,
-        currnetApplicantInfo: null,
-        // currnetApplicantInfo: {
-        //   submitted_applicant: {
-        //     status: {
-        //       is_printed_arrived: action.payload.submitted_applicant.status.is_printed_arrived,
-        //       is_submit: action.payload.submitted_applicant.status.is_submit,
-        //     },
-        //     personal_data: {
-        //       photo_file_name: action.payload.submitted_applicant.personal_data.photo_file_name,
-        //       name: action.payload.submitted_applicant.personal_data.name,
-        //       birth_date: action.payload.submitted_applicant.personal_data.birth_date,
-        //       school_name: action.payload.submitted_applicant.personal_data.school_name,
-        //       is_graduated: action.payload.submitted_applicant.personal_data.is_graduated,
-        //       educational_status: action.payload.submitted_applicant.personal_data.educational_status,
-        //       application_type: action.payload.submitted_applicant.personal_data.application_type,
-        //       address: action.payload.submitted_applicant.personal_data.address,
-        //       detail_address: action.payload.submitted_applicant.personal_data.detail_address,
-        //       email: action.payload.submitted_applicant.personal_data.email,
-        //       telephone_number: action.payload.submitted_applicant.personal_data.telephone_number,
-        //       parent_tel: action.payload.submitted_applicant.personal_data.parent_tel,
-        //       school_tel: action.payload.submitted_applicant.personal_data.school_name,
-        //       home_tel: action.payload.submitted_applicant.personal_data.home_tel,
-        //     },
-        //     evaluation: {
-        //       volunteer_time: action.payload.submitted_applicant.evaluation.volunteer_time,
-        //       conversion_score: action.payload.submitted_applicant.evaluation.conversion_score,
-        //       day_absence_count: action.payload.submitted_applicant.evaluation.day_absence_count,
-        //       lecture_absence_count: action.payload.submitted_applicant.evaluation.lecture_absence_count,
-        //       early_leave_count: action.payload.submitted_applicant.evaluation.early_leave_count,
-        //       lateness_count: action.payload.submitted_applicant.evaluation.lateness_count,
-        //       self_introduce: action.payload.submitted_applicant.evaluation.self_introduce,
-        //       study_plan: action.payload.submitted_applicant.evaluation.study_plan,
-        //     },
-        //   },
-        //   not_submitted_applicant: {
-        //     email: action.payload.not_submitted_applicant.email,
-        //     telephone_number: action.payload.not_submitted_applicant.telephone_number,
-        //     parent_tel: action.payload.not_submitted_applicant.parent_tel,
-        //     home_tel: action.payload.not_submitted_applicant.home_tel,
-        //     school_tel: action.payload.not_submitted_applicant.school_tel,
-        //   }
-        // },
+        // currnetApplicantInfo: null,
+        currnetApplicantInfo: {
+          submitted_applicant: {
+            status: action.payload.submitted_applicant.status,
+            personal_data: action.payload.submitted_applicant.personal_data,
+            evaluation: action.payload.submitted_applicant.evaluation
+          },
+        },
       };
     }
     case GET_APPLICANT_INFO_FAILURE:
       return {
         ...state,
-        error: action.payload,
+        currnetApplicantInfo: {
+          not_submitted_applicant: {
+            email: action.payload.not_submitted_applicant.email,
+            applicant_tel: action.payload.not_submitted_applicant.applicant_tel,
+            parent_tel: action.payload.not_submitted_applicant.parent_tel,
+            home_tel: action.payload.not_submitted_applicant.home_tel,
+            school_tel: action.payload.not_submitted_applicant.school_tel,
+          }
+        }
       };
     case UPDATE_APPLICANT_STATUS: 
       return {
