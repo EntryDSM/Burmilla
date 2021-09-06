ddc4ee2f13bc9d6f558a24f6471d37a40fd96770
Hyeonyee <jgh10070@naver.com>
2021-09-06 23:55:46 +0900

    [FIX] useApplicant

--- src/hooks/applicant/useApplicant.ts
+++ src/hooks/applicant/useApplicant.ts
@@ -2,6 +2,7 @@ import { useDispatch } from 'react-redux';
 import { useSelectState } from '../default';
 import {
   setFilter,
+  setApplicantInfoAppear,
   getApplicantsList,
   getApplicantInfo,
   updateApplicantStatus,
@@ -19,6 +20,7 @@ const useApplicant = () => {
   const state = useSelectState().applicant;
   const setState = {
     setFilter: (payload: GetApplicantsListPayload) => dispatch(setFilter(payload)),
+    setApplicantInfoAppear: (payload: boolean) => dispatch(setApplicantInfoAppear(payload)),
     getApplicantsList: (payload: GetApplicantsListPayload) => dispatch(getApplicantsList(payload)),
     getApplicantInfo: (payload: GetApplicantInfoPayload) => dispatch(getApplicantInfo(payload)),
     updateApplicantStatus: (payload: UpdateApplicantStatusPayload) => dispatch(updateApplicantStatus(payload)),
