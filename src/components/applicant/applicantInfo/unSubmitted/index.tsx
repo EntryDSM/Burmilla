56a435de1986956ac03948dd079284d7d7656e08
Hyeonyee <jgh10070@naver.com>
2021-09-07 23:55:55 +0900

    [FIX] 세부 정보 모달 닫는 버튼 만듬

--- src/components/applicant/applicantInfo/unSubmitted/index.tsx
+++ src/components/applicant/applicantInfo/unSubmitted/index.tsx
@@ -1,7 +1,10 @@
 import React, { FC } from "react";
 import * as S from "./style";
+import { details_arrow } from "../../../../assets/applicants";
+import { returnContact } from "../../../../utils/checkType";
 
 interface Props {
+  applicantInfoAppear: boolean;
   not_submitted_applicant: {
     email: string;
     applicant_tel: string;
@@ -9,9 +12,11 @@ interface Props {
     school_tel?: string;
     home_tel?: string;
   };
+  setApplicantInfoAppear: (payload: boolean) => void;
 }
 
 const Unsubmitted: FC<Props> = ({
+  applicantInfoAppear,
   not_submitted_applicant: {
     email,
     applicant_tel,
@@ -19,7 +24,16 @@ const Unsubmitted: FC<Props> = ({
     home_tel,
     school_tel,
   },
+  setApplicantInfoAppear,
 }) => {
+  const setContactForm = React.useCallback((contact: string) => {
+    return returnContact(contact);
+  }, []);
+
+  const handleClickDetailArrow = () => {
+    setApplicantInfoAppear(!applicantInfoAppear);
+  };
+
   const checkIsSubmitted = () => {
     if (school_tel !== null) {
       return (
@@ -30,19 +44,19 @@ const Unsubmitted: FC<Props> = ({
           </S.BasicInfoTableTR>
           <S.BasicInfoTableTR>
             <th>학생 연락처</th>
-            <td>{applicant_tel}</td>
+            <td>{setContactForm(applicant_tel)}</td>
           </S.BasicInfoTableTR>
           <S.BasicInfoTableTR>
             <th>부모님 연락처</th>
-            <td>{parent_tel}</td>
+            <td>{setContactForm(parent_tel)}</td>
           </S.BasicInfoTableTR>
           <S.BasicInfoTableTR>
             <th>학교 연락처</th>
-            <td>{school_tel}</td>
+            <td>{setContactForm(school_tel)}</td>
           </S.BasicInfoTableTR>
           <S.BasicInfoTableTR>
             <th>자택 연락처</th>
-            <td>{home_tel}</td>
+            <td>{setContactForm(home_tel)}</td>
           </S.BasicInfoTableTR>
         </>
       );
@@ -56,15 +70,15 @@ const Unsubmitted: FC<Props> = ({
           </S.BasicInfoTableTR>
           <S.BasicInfoTableTR>
             <th>학생 연락처</th>
-            <td>{applicant_tel}</td>
+            <td>{setContactForm(applicant_tel)}</td>
           </S.BasicInfoTableTR>
           <S.BasicInfoTableTR>
             <th>부모님 연락처</th>
-            <td>{parent_tel}</td>
+            <td>{setContactForm(parent_tel)}</td>
           </S.BasicInfoTableTR>
           <S.BasicInfoTableTR>
             <th>자택 연락처</th>
-            <td>{home_tel}</td>
+            <td>{setContactForm(home_tel)}</td>
           </S.BasicInfoTableTR>
         </>
       );
@@ -73,6 +87,10 @@ const Unsubmitted: FC<Props> = ({
 
   return (
     <S.Wrapper>
+      <S.DetailInfo
+        src={details_arrow}
+        onClick={() => handleClickDetailArrow()}
+      />
       <h3>최종제출을 하지 않은 지원자입니다.</h3>
       <table>
         <tbody>{checkIsSubmitted()}</tbody>
