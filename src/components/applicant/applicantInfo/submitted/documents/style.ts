4c02ec200c87744d195b8550c30481b65df034b5
Hyeonyee <jgh10070@naver.com>
2021-09-07 21:16:07 +0900

    [FIX] submitted documents style 수정

--- src/components/applicant/applicantInfo/submitted/documents/style.ts
+++ src/components/applicant/applicantInfo/submitted/documents/style.ts
@@ -1,30 +1,65 @@
 import styled from 'styled-components';
-
 import { color } from '../../../../../styles';
 
 export const Wrapper = styled.div`
-  margin-top: 36px;
+  margin-top: 34px;
 `;
 
 export const ContentSelector = styled.div`
   border-bottom: solid 1px ${color.primaryColor};
   > span {
-    font-size: 16px;
-    padding: 10px;
+    width: 85px;
+    font-size: 14px;
+    font-weight: bold;
+    text-align: center;
+    padding: 10px 0 2.5px 0px;
     display: inline-block;
+    position: relative;
     cursor: pointer;
 
     &.selected {
-      border-bottom: solid 4px ${color.primaryColor};
+      font-family: 'Noto Sans KR-Medium', sans-serif !important;
     }
+    
+    > div {
+      height: 3.5px;
+      width: 1px;
+      border-radius: 4px 4px 0px 0px;
+      visibility: hidden;
+      position: absolute;
+      background-color: ${color.primaryColor};
+      /* transition: */
+
+      &.selected {
+        width: 100%;
+        visibility: visible;
+      }
+    }
+  }
+  > span:last-child {
+    margin-left: 12px;
   }
 `;
 
 export const Document = styled.div`
-  height: 200px;
-  overflow-y: auto;
+  height: 125px;
+  overflow-y: scroll;
   font-weight: 300;
-  font-size: 16px;
+  font-size: 14px;
+  font-weight: bold;
   line-height: 2.25;
-  margin-top: 20px;
+  margin-top: 8px;
+  padding: 0px 4px;
+
+  &::-webkit-scrollbar {
+		width: 6px;
+		background: none;
+	}
+	&::-webkit-scrollbar-thumb {
+	  background: ${color.primaryColor};
+    border-radius: 6px;
+	}
+	&::-webkit-scrollbar-track {
+	  background: none;
+	}
 `;
