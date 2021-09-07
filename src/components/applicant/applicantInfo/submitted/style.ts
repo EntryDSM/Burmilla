6d7f7a856a72caee0bada23e4e14c7676eae366b
Hyeonyee <jgh10070@naver.com>
2021-09-07 23:56:16 +0900

    [FIX] submitted 모달 style바꿈

--- src/components/applicant/applicantInfo/submitted/style.ts
+++ src/components/applicant/applicantInfo/submitted/style.ts
@@ -1,6 +1,9 @@
 import styled from 'styled-components';
+import { color } from "../../../../styles";
 
 export const Wrapper = styled.div`
-  background-color: white;
+  background-color: ${color.backgorund};
   padding: 30px 68px 18px 68px;
+  border-left: 2.5px solid #f0f0f0;
+  transition: all ease-out 0.4s;
 `;
