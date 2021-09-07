05156558cd13540c130c22d35e4ef8ef8067f672
Hyeonyee <jgh10070@naver.com>
2021-09-07 23:55:49 +0900

    [FIX] 세부 정보 모달 닫는 버튼 만듬

--- src/components/applicant/applicantInfo/unSubmitted/style.ts
+++ src/components/applicant/applicantInfo/unSubmitted/style.ts
@@ -1,10 +1,21 @@
 import styled from 'styled-components';
 import { color } from '../../../../styles';
 
+export const DetailInfo = styled.img`
+  position: absolute;
+  top: 18px;
+  left: 22px;
+
+  &:hover {
+    cursor: pointer;
+  }
+`;
+
 export const Wrapper = styled.div`
   padding: 136.5px 72px;
   height: 28rem;
   background-color: ${color.backgorund};
+  border-left: 2.5px solid #f0f0f0;
 
   > h3 {
     font-size: 26px;
