bfda7cf3023e083592e41dd305ba0ad5ce188b14
Hyeonyee <jgh10070@naver.com>
2021-09-09 23:58:17 +0900

    [FIX] applicantStatus style

--- src/components/applicant/applicantInfo/submitted/applicantStatus/style.ts
+++ src/components/applicant/applicantInfo/submitted/applicantStatus/style.ts
@@ -3,6 +3,8 @@ import { color } from '../../../../../styles/index';
 
 export const Wrapper = styled.div`
   display: flex;
+  width: 100%;
+  min-width: 28rem;
 `;
 
 export const DetailInfo = styled.img`
@@ -18,7 +20,7 @@ export const DetailInfo = styled.img`
 export const CheckboxContainer = styled.div`
   display: flex;
   align-items: center;
-  margin-right: 36px;
+  width: 25%;
 
   &:last-child {
     margin-right: 60px;
@@ -26,7 +28,15 @@ export const CheckboxContainer = styled.div`
 
   > p {
     font-size: 14px;
+    font-weight: bold;
     color: ${color.lightBorderColor};
-    margin-left: 18px;
+    margin-left: 12px;
   }
+`;
+
+export const ButtonContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-evenly;
+  width: 50%;
 `;
