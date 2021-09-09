0a22f2a4aa4167d3887cf8396b1ee2264c1886ca
Hyeonyee <jgh10070@naver.com>
2021-09-09 23:53:38 +0900

    [FIX] applicant acton

--- src/data/modules/redux/action/applicant/index.ts
+++ src/data/modules/redux/action/applicant/index.ts
@@ -1,6 +1,5 @@
 import { 
   SET_FILTER,
-  SET_APPLICANT_INFO_APPEAR,
   GET_APPLICANT_INFO,
   GET_APPLICANT_INFO_SUCCESS,
   GET_APPLICANT_INFO_FAILURE,
@@ -13,10 +12,12 @@ import {
   UPDATE_APPLICANT_SUBMIT_STATUS,
   UPDATE_APPLICANT_SUBMIT_STATUS_SUCCESS,
   UPDATE_APPLICANT_SUBMIT_STATUS_FAILURE,
-  UPDATE_APPLICANT_LIST,
-  UPDATE_APPLICANT_LIST_SUCCESS,
-  UPDATE_APPLICANT_LIST_FAILURE,
-  RESET_UPDATE_STATUS,
+  CHECK_PASSWORD,
+  CHECK_PASSWORD_SUCCESS,
+  CHECK_PASSWORD_FAILURE,
+  DELETE_APPLICANT_TABLE,
+  DELETE_APPLICANT_TABLE_SUCCESS,
+  DELETE_APPLICANT_TABLE_FAILURE,
 } from "./interface";
 import { 
   GetApplicantsListPayload,
@@ -24,12 +25,13 @@ import {
   GetApplicantInfoPayload,
   GetApplicantInfoResponse,
   UpdateApplicantStatusPayload,
+  UpdateApplicantSubmitStatusPayload,
+  CheckPasswordRequest
  } from "../../../../api/apiTypes"; 
 import { error } from "../../../../../models/error";
 import { createAction } from 'typesafe-actions';
 
 export const setFilter = createAction(SET_FILTER)<GetApplicantsListPayload>();
-export const setApplicantInfoAppear = createAction(SET_APPLICANT_INFO_APPEAR)<boolean>();
 export const getApplicantInfo = createAction(GET_APPLICANT_INFO)<GetApplicantInfoPayload>();
 export const getApplicantInfoSuccess = createAction(GET_APPLICANT_INFO_SUCCESS)<GetApplicantInfoResponse>();
 export const getApplicantInfoFailure = createAction(GET_APPLICANT_INFO_FAILURE)<GetApplicantInfoResponse>();
@@ -39,19 +41,18 @@ export const getApplicantsListFailure = createAction(GET_APPLICANTS_LIST_FAILURE
 export const updateApplicantStatus = createAction(UPDATE_APPLICANT_STATUS)<UpdateApplicantStatusPayload>();
 export const updateApplicantStatusSuccess = createAction(UPDATE_APPLICANT_STATUS_SUCCESS)();
 export const updateApplicantStatusFailure = createAction(UPDATE_APPLICANT_STATUS_FAILURE)<error>();
-
-export const updateApplicantSubmitStatus = createAction(UPDATE_APPLICANT_SUBMIT_STATUS)<string>();
-export const updateApplicantSubmitStatusSuccess = createAction(UPDATE_APPLICANT_SUBMIT_STATUS_SUCCESS)<string>();
-export const updateApplicantSubmitStatusFailure = createAction(UPDATE_APPLICANT_SUBMIT_STATUS_FAILURE)<string>();
-export const updateApplicantList = createAction(UPDATE_APPLICANT_LIST)<string>();
-export const updateApplicantListSuccess = createAction(UPDATE_APPLICANT_LIST_SUCCESS)<string>();
-export const updateApplicantListFailure = createAction(UPDATE_APPLICANT_LIST_FAILURE)<string>();
-
-export const resetUpdateStatus = createAction(RESET_UPDATE_STATUS)<null>();
+export const updateApplicantSubmitStatus = createAction(UPDATE_APPLICANT_SUBMIT_STATUS)<UpdateApplicantSubmitStatusPayload>();
+export const updateApplicantSubmitStatusSuccess = createAction(UPDATE_APPLICANT_SUBMIT_STATUS_SUCCESS)();
+export const updateApplicantSubmitStatusFailure = createAction(UPDATE_APPLICANT_SUBMIT_STATUS_FAILURE)<error>();
+export const checkPassword = createAction(CHECK_PASSWORD)<CheckPasswordRequest>();
+export const checkPasswordSuccess = createAction(CHECK_PASSWORD_SUCCESS)<boolean>();
+export const checkPasswordFailure = createAction(CHECK_PASSWORD_FAILURE)<error>();
+export const deleteApplicantTable = createAction(DELETE_APPLICANT_TABLE)();
+export const deleteApplicantTableSuccess = createAction(DELETE_APPLICANT_TABLE_SUCCESS)<boolean>();
+export const deleteApplicantTableFailure = createAction(DELETE_APPLICANT_TABLE_FAILURE)<error>();
 
 export type applicantActionType = 
   | ReturnType<typeof setFilter>
-  | ReturnType<typeof setApplicantInfoAppear>
   | ReturnType<typeof getApplicantInfo>
   | ReturnType<typeof getApplicantInfoSuccess>
   | ReturnType<typeof getApplicantInfoFailure>
@@ -61,4 +62,12 @@ export type applicantActionType =
   | ReturnType<typeof updateApplicantStatus>
   | ReturnType<typeof updateApplicantStatusSuccess>
   | ReturnType<typeof updateApplicantStatusFailure>
-  | ReturnType<typeof resetUpdateStatus>;
+  | ReturnType<typeof updateApplicantSubmitStatus>
+  | ReturnType<typeof updateApplicantSubmitStatusSuccess>
+  | ReturnType<typeof updateApplicantSubmitStatusFailure>
+  | ReturnType<typeof checkPassword>
+  | ReturnType<typeof checkPasswordSuccess>
+  | ReturnType<typeof checkPasswordFailure>
+  | ReturnType<typeof deleteApplicantTable>
+  | ReturnType<typeof deleteApplicantTableSuccess>
+  | ReturnType<typeof deleteApplicantTableFailure>
