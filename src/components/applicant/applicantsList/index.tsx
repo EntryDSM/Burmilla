8c264b0c44fe527046309106a37e458d76009e9d
Hyeonyee <jgh10070@naver.com>
2021-09-09 23:56:42 +0900

    [FIX] applicantList

--- src/components/applicant/applicantsList/index.tsx
+++ src/components/applicant/applicantsList/index.tsx
@@ -10,25 +10,25 @@ import {
 } from "../../../data/api/apiTypes";
 
 interface Props {
-  applicantInfoAppear: boolean;
+  isContainerWidth: boolean;
   filters: GetApplicantsListPayload;
   currnetApplicantInfo: GetApplicantInfoResponse;
   applicantsList: GetApplicantsListResponse;
-  setApplicantInfoAppear: (payload: boolean) => void;
+  setIsContainerWidth: (payload: boolean) => void;
   getApplicantInfo: (payload: GetApplicantInfoPayload) => void;
 }
 
 const ApplicantsList: FC<Props> = ({
-  applicantInfoAppear,
+  isContainerWidth,
   filters,
   currnetApplicantInfo,
   applicantsList,
-  setApplicantInfoAppear,
+  setIsContainerWidth,
   getApplicantInfo,
 }) => {
   const handleClickListItem = React.useCallback((receipt_code: number) => {
     getApplicantInfo({ receipt_code });
-    setApplicantInfoAppear(!applicantInfoAppear);
+    setIsContainerWidth(!isContainerWidth);
   }, []);
 
   return (
@@ -39,8 +39,6 @@ const ApplicantsList: FC<Props> = ({
           applicantsList.applicants_information_responses.map((info) => (
             <ApplicantsListItem
               currnetApplicantInfo={currnetApplicantInfo}
-              // currnetApplicantInfoSuccess={currnetApplicantInfoSuccess}
-              // currnetApplicantInfoFailure={currnetApplicantInfoFailure}
               key={info.receipt_code}
               applicantInfo={info}
               handleClick={handleClickListItem}
