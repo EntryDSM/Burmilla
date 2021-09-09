6c0d983c7266a48a45563f5c627d5642e47e273e
Hyeonyee <jgh10070@naver.com>
2021-09-09 23:51:56 +0900

    [FIX] applicant middleware

--- src/data/modules/redux/middleware/applicant/index.ts
+++ src/data/modules/redux/middleware/applicant/index.ts
@@ -1,16 +1,18 @@
 import { takeLatest } from 'redux-saga/effects';
 import createRequestSaga from '../../../../../utils/saga/createRequestSaga';
-import { getApplicantsListApi, getApplicantInfoApi, updateApplicantStatusApi } from '../../../../api/index';
-import { GET_APPLICANTS_LIST, GET_APPLICANT_INFO, UPDATE_APPLICANT_STATUS } from '../../../../modules/redux/action/applicant/interface';
+import { getApplicantsListApi, getApplicantInfoApi, updateApplicantStatusApi, updateApplicantSubmitStatusApi } from '../../../../api/index';
+import { GET_APPLICANTS_LIST, GET_APPLICANT_INFO, UPDATE_APPLICANT_STATUS, UPDATE_APPLICANT_SUBMIT_STATUS } from '../../../../modules/redux/action/applicant/interface';
 
 export const getApplicantListSaga = createRequestSaga(GET_APPLICANTS_LIST, getApplicantsListApi);
 export const getApplicantInfoSaga = createRequestSaga(GET_APPLICANT_INFO, getApplicantInfoApi);
 export const updateApplicantStatusSaga = createRequestSaga(UPDATE_APPLICANT_STATUS, updateApplicantStatusApi);
+export const updateApplicantSubmitStatusSaga = createRequestSaga(UPDATE_APPLICANT_SUBMIT_STATUS, updateApplicantSubmitStatusApi);
 
 function* applicantSaga() {
   yield takeLatest(GET_APPLICANTS_LIST, getApplicantListSaga);
   yield takeLatest(GET_APPLICANT_INFO, getApplicantInfoSaga);
   yield takeLatest(UPDATE_APPLICANT_STATUS, updateApplicantStatusSaga);
+  yield takeLatest(UPDATE_APPLICANT_SUBMIT_STATUS, updateApplicantSubmitStatusSaga);
 }
 
 export default applicantSaga;
