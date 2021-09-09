20aeda4ccc15900ce7269d78b2d8c3180f297720
Hyeonyee <jgh10070@naver.com>
2021-09-09 23:55:44 +0900

    [FIX] pagination

--- src/components/applicant/pagination/Pagination.tsx
+++ src/components/applicant/pagination/Pagination.tsx
@@ -2,6 +2,8 @@ import React, { FC } from "react";
 import * as S from "./style";
 import { shift_left_icon, shift_right_icon } from "../../../assets/applicants";
 import { getIndexList } from "../../../utils/pagination";
+import { SET_FILTER } from "../../../data/modules/redux/action/applicant/interface";
+import { useDispatch } from "react-redux";
 import {
   GetApplicantsListPayload,
   GetApplicantsListResponse,
@@ -17,6 +19,8 @@ const Pagination: FC<Props> = ({ applicantsList, filters, setFilter }) => {
   const [currentIndex, setCurrentIndex] = React.useState(1);
   const [indexList, setIndexList] = React.useState<number[]>([]);
 
+  const dispatch = useDispatch();
+
   React.useEffect(() => {
     setIndexList(getIndexList(currentIndex, applicantsList.total_pages));
   }, [currentIndex, applicantsList.total_pages]);
@@ -25,31 +29,53 @@ const Pagination: FC<Props> = ({ applicantsList, filters, setFilter }) => {
     setCurrentIndex(0);
   }, [applicantsList.total_pages]);
 
-  const handleClickIndex = React.useCallback((index: number) => {
-    setCurrentIndex(index);
-    setFilter({
-      page: index - 1,
-    });
-  }, []);
+  const handleClickIndex = React.useCallback(
+    (index: number) => {
+      setCurrentIndex(index);
+      setFilter({
+        page: index - 1,
+      });
+      // dispatch({ type: SET_FILTER });
+    },
+    [setFilter, dispatch]
+  );
+
   const handleClickPrev = React.useCallback(() => {
     if (currentIndex > 1) setCurrentIndex(currentIndex - 1);
     setFilter({
       page: currentIndex,
     });
-  }, [currentIndex]);
+    dispatch({ type: SET_FILTER });
+  }, [currentIndex, SET_FILTER]);
+
   const handleClickNext = React.useCallback(() => {
     if (currentIndex < applicantsList.total_pages)
       setCurrentIndex(currentIndex + 1);
     setFilter({
       page: currentIndex,
     });
-  }, [currentIndex, applicantsList.total_pages]);
+    dispatch({ type: SET_FILTER });
+  }, [currentIndex, applicantsList.total_pages, SET_FILTER]);
 
-  return (
-    <S.PaginationContainer className="no-select">
+  const nextBtn = React.useMemo(() => {
+    return (
+      <S.PageBtn className="move-btn" onClick={handleClickNext}>
+        <img src={shift_right_icon} alt="shift_right" />
+      </S.PageBtn>
+    );
+  }, [filters]);
+
+  const prevBtn = React.useMemo(() => {
+    return (
       <S.PageBtn className="move-btn" onClick={handleClickPrev}>
         <img src={shift_left_icon} alt="shift_left" />
       </S.PageBtn>
+    );
+  }, [filters]);
+
+  return (
+    <S.PaginationContainer className="no-select">
+      {prevBtn}
       {indexList.map((i) => (
         <S.PageBtn
           key={i.toString()}
@@ -59,9 +85,7 @@ const Pagination: FC<Props> = ({ applicantsList, filters, setFilter }) => {
           {i}
         </S.PageBtn>
       ))}
-      <S.PageBtn className="move-btn" onClick={handleClickNext}>
-        <img src={shift_right_icon} alt="shift_right" />
-      </S.PageBtn>
+      {nextBtn}
     </S.PaginationContainer>
   );
 };
