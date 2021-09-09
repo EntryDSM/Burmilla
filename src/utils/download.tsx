d2fba2a40c947d1df14b72f8fe1c7567ed2e2a70
Hyeonyee <jgh10070@naver.com>
2021-09-09 23:44:12 +0900

    [FIX] download fix

--- src/utils/download.tsx
+++ src/utils/download.tsx
@@ -1,15 +1,15 @@
 export const downloadExcel = async (api: () => Promise<any>, title: string) => {
-    const response = await api();
-  
-    const currentTime = new Date();
-  
-    const link = document.createElement('a');
-    link.href = URL.createObjectURL(new Blob([response])).toString();
-    link.setAttribute(
-      'download',
-      `${title}_${currentTime.getMonth() +
-        1}월${currentTime.getDate()}일_${currentTime.getHours()}시${currentTime.getMinutes()}분.xlsx`,
-    );
-    link.click();
-  };
-  
+  const response = await api();
+
+  const currentTime = new Date();
+
+  const link = document.createElement("a");
+  link.href = URL.createObjectURL(new Blob([response])).toString();
+  link.setAttribute(
+    "download",
+    `${title}_${
+      currentTime.getMonth() + 1
+    }월${currentTime.getDate()}일_${currentTime.getHours()}시${currentTime.getMinutes()}분.xls`
+  );
+  link.click();
+};
