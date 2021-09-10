5bc469055e0e79f22131f904aced16e526f08941
Hyeonyee <jgh10070@naver.com>
2021-09-10 23:49:29 +0900

    [FIX] filter search props 넘김

--- src/components/applicant/filterSearch/index.tsx
+++ src/components/applicant/filterSearch/index.tsx
@@ -3,14 +3,30 @@ import { list_progressbar, search_icon } from "../../../assets/applicants";
 import SearchBar from "./searchBar";
 import Filter from "./filter";
 import FilterSearchWrapper from "./style";
-import { GetApplicantsListPayload } from "../../../data/api/apiTypes";
+import {
+  GetApplicantsListPayload,
+  CheckPasswordRequest,
+} from "../../../data/api/apiTypes";
 
 interface Props {
   filters: GetApplicantsListPayload;
+  isDeleteTableModalSwitch: boolean;
+  isCheckPassword: boolean;
   setFilter: (payload: GetApplicantsListPayload) => void;
+  checkPassword: (payload: CheckPasswordRequest) => void;
+  deleteApplicantTable: () => void;
+  setIsDeleteTableModalSwitch: (payload: boolean) => void;
 }
 
-const FilterSearch: FC<Props> = ({ filters, setFilter }) => {
+const FilterSearch: FC<Props> = ({
+  filters,
+  isCheckPassword,
+  isDeleteTableModalSwitch,
+  setFilter,
+  checkPassword,
+  deleteApplicantTable,
+  setIsDeleteTableModalSwitch,
+}) => {
   return (
     <FilterSearchWrapper>
       <SearchBar
@@ -18,7 +34,16 @@ const FilterSearch: FC<Props> = ({ filters, setFilter }) => {
         searchIcon={search_icon}
         setFilter={setFilter}
       />
-      <Filter filters={filters} setFilter={setFilter} />
+      <Filter
+        disable={!isCheckPassword}
+        filters={filters}
+        isCheckPassword={isCheckPassword}
+        isDeleteTableModalSwitch={isDeleteTableModalSwitch}
+        setFilter={setFilter}
+        deleteApplicantTable={deleteApplicantTable}
+        checkPassword={checkPassword}
+        setIsDeleteTableModalSwitch={setIsDeleteTableModalSwitch}
+      />
     </FilterSearchWrapper>
   );
 };
