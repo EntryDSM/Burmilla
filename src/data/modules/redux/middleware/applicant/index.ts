9a495837b94616a6ad709a892fd09e294fad58c0
Hyeonyee <jgh10070@naver.com>
2021-09-10 23:50:34 +0900

    [FIX] middleware 추가

--- src/data/modules/redux/middleware/applicant/index.ts
+++ src/data/modules/redux/middleware/applicant/index.ts
@@ -1,12 +1,14 @@
 import { takeLatest } from 'redux-saga/effects';
 import createRequestSaga from '../../../../../utils/saga/createRequestSaga';
-import { getApplicantsListApi, getApplicantInfoApi, updateApplicantStatusApi, updateApplicantSubmitStatusApi } from '../../../../api/index';
-import { GET_APPLICANTS_LIST, GET_APPLICANT_INFO, UPDATE_APPLICANT_STATUS, UPDATE_APPLICANT_SUBMIT_STATUS } from '../../../../modules/redux/action/applicant/interface';
+import { getApplicantsListApi, getApplicantInfoApi, updateApplicantStatusApi, updateApplicantSubmitStatusApi, checkPasswordApi, deleteApplicantTableApi } from '../../../../api/index';
+import { GET_APPLICANTS_LIST, GET_APPLICANT_INFO, UPDATE_APPLICANT_STATUS, UPDATE_APPLICANT_SUBMIT_STATUS, CHECK_PASSWORD, DELETE_APPLICANT_TABLE } from '../../../../modules/redux/action/applicant/interface';
 
 export const getApplicantListSaga = createRequestSaga(GET_APPLICANTS_LIST, getApplicantsListApi);
 export const getApplicantInfoSaga = createRequestSaga(GET_APPLICANT_INFO, getApplicantInfoApi);
 export const updateApplicantStatusSaga = createRequestSaga(UPDATE_APPLICANT_STATUS, updateApplicantStatusApi);
 export const updateApplicantSubmitStatusSaga = createRequestSaga(UPDATE_APPLICANT_SUBMIT_STATUS, updateApplicantSubmitStatusApi);
+export const checkPasswordSaga = createRequestSaga(CHECK_PASSWORD, checkPasswordApi);
+export const deleteApplicantTableSaga = createRequestSaga(DELETE_APPLICANT_TABLE, deleteApplicantTableApi);
 
 function* applicantSaga() {
   yield takeLatest(GET_APPLICANTS_LIST, getApplicantListSaga);
