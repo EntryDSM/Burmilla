899e0908e402f2c973ba9a664854df0a7fb378b8
Hyeonyee <jgh10070@naver.com>
2021-09-11 14:22:01 +0900

    [FIX] deleteTable

--- src/components/applicant/deleteTable/index.tsx
+++ src/components/applicant/deleteTable/index.tsx
@@ -1,12 +1,18 @@
 import React, { FC } from "react";
+import { useHistory } from "react-router";
 import * as S from "./style";
 import { close_modal } from "../../../assets/applicants";
-import { CheckPasswordRequest } from "../../../data/api/apiTypes";
+import { display_icon, hide_icon } from "../../../assets/login";
+import {
+  GetApplicantsListResponse,
+  CheckPasswordRequest,
+} from "../../../data/api/apiTypes";
 
 interface Props {
   disable: boolean;
-  isCheckPassword: boolean;
-  isDeleteTableModalSwitch: boolean;
+  applicantsList: GetApplicantsListResponse;
+  password: string;
+  setPassword: (payload: string) => void;
   checkPassword: (payload: CheckPasswordRequest) => void;
   deleteApplicantTable: () => void;
   setIsDeleteTableModalSwitch: (payload: boolean) => void;
@@ -14,28 +20,85 @@ interface Props {
 
 const DeleteTable: FC<Props> = ({
   disable,
-  isCheckPassword,
-  isDeleteTableModalSwitch,
+  applicantsList,
+  password,
+  setPassword,
   checkPassword,
   deleteApplicantTable,
   setIsDeleteTableModalSwitch,
 }) => {
+  const [isPasswordShown, setIsPasswordShown] = React.useState<boolean>(false);
+
+  const history = useHistory();
+
+  React.useEffect(() => {
+    checkPassword({ password });
+  }, [password]);
+
+  const handleClickOffModal = React.useCallback(() => {
+    setIsDeleteTableModalSwitch(false);
+  }, []);
+
+  const handleChangePassword = React.useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      setPassword(e.target.value);
+    },
+    []
+  );
+
+  const togglePasswordVisiblity = () => {
+    setIsPasswordShown(!isPasswordShown);
+  };
+
+  const handleClickDeleteTable = React.useCallback(() => {
+    if (disable === true) {
+      deleteApplicantTable();
+    }
+    return false;
+  }, []);
+
+  const keyPressHandler = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+    }
+  };
+
   return (
     <S.ModalWrapper>
       <S.ModalDeleteTabel>
         <S.ModalDeleteImg>
-          <S.Img src={close_modal} alt="모달 닫기" />
+          <S.CloseModalImg
+            src={close_modal}
+            onClick={handleClickOffModal}
+            alt="모달 닫기"
+          />
         </S.ModalDeleteImg>
         <S.ModalTitle>정말 삭제하시겠습니까?</S.ModalTitle>
         <S.ModalSubTitle>
           삭제하면 다시 수정할 수 없으니 다시 한번 확인해 주세요
         </S.ModalSubTitle>
         <S.FormBox>
+          {password && !disable ? (
+            <span>비밀번호가 일치하지 않습니다</span>
+          ) : (
+            ""
+          )}
           <S.InputBox>
-            <S.Input />
+            <S.Input
+              type={isPasswordShown ? "text" : "password"}
+              placeholder="비밀번호를 입력해주세요"
+              value={password}
+              onChange={handleChangePassword}
+              onKeyPress={keyPressHandler}
+            />
+            <S.CheckDisplayImg
+              src={isPasswordShown ? hide_icon : display_icon}
+              title={isPasswordShown ? "문자 숨기기" : "문자 보이기"}
+              alt="show-icon"
+              onClick={togglePasswordVisiblity}
+            />
           </S.InputBox>
-          <S.DeleteButton>
-            <S.Button />
+          <S.DeleteButton disable={disable}>
+            <S.Button onClick={handleClickDeleteTable}>삭제</S.Button>
           </S.DeleteButton>
         </S.FormBox>
       </S.ModalDeleteTabel>
