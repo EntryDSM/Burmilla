c3c4e364105c5d68bda3015cb5ed82fbd74992e7
Hyeonyee <jgh10070@naver.com>
2021-09-12 00:08:00 +0900

    [FIX] 검정고시 테이블 수정

--- src/components/applicant/applicantInfo/submitted/detailInfo/QualificationexamDetailInfo.tsx
+++ src/components/applicant/applicantInfo/submitted/detailInfo/QualificationexamDetailInfo.tsx
@@ -5,7 +5,7 @@ import {
   ApplicantPersonalData,
   ApplicantEvaluation,
 } from "../../../../../data/api/apiTypes";
-import { returnContact } from "../../../../../utils/checkType";
+import { phoneWithHyphen } from "../../../../../utils/checkType";
 
 interface Props {
   applicantPersonalData: ApplicantPersonalData;
@@ -17,25 +17,29 @@ const DetailInfo: FC<Props> = ({
   applicantEvaluation: { conversion_score },
 }) => {
   const setContactForm = React.useCallback((contact: string) => {
-    return returnContact(contact);
+    return phoneWithHyphen(contact);
   }, []);
 
   return (
-    <S.DetailInfoTable>
+    <S.QualificationexamDetailInfoTable>
       <tbody>
         <tr>
           <th>학생 연락처</th> <td>{setContactForm(telephone_number)}</td>
+        </tr>
+        <tr>
           <th>부모님 연락처</th> <td>{setContactForm(parent_tel)}</td>
         </tr>
         <tr>
           <th>이메일</th> <td>{email}</td>
+        </tr>
+        <tr>
           <th>자택 연락처</th> <td>{setContactForm(home_tel)}</td>
         </tr>
         <tr>
           <th>검정고시 평균 점수</th> <td>{conversion_score}점</td>
         </tr>
       </tbody>
-    </S.DetailInfoTable>
+    </S.QualificationexamDetailInfoTable>
   );
 };
 export default DetailInfo;
