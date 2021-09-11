42b05570a81c8d82db143bd08f24e4e93c062ef4
Hyeonyee <jgh10070@naver.com>
2021-09-12 00:07:25 +0900

    [FIX] submitted style

--- src/components/applicant/applicantInfo/submitted/style.ts
+++ src/components/applicant/applicantInfo/submitted/style.ts
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 import { color } from "../../../../styles";
 
 export const Wrapper = styled.div`
+  height: 100%;
   background-color: ${color.backgorund};
   padding: 38px 68px 18px 68px;
   border-left: 2.5px solid #f0f0f0;
