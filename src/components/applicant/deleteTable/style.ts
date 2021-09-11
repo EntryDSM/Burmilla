ac8bddb774524f556b622ccf4ce633a69908c086
Hyeonyee <jgh10070@naver.com>
2021-09-11 14:21:52 +0900

    [FIX] deleteTable style 설정

--- src/components/applicant/deleteTable/style.ts
+++ src/components/applicant/deleteTable/style.ts
@@ -1,5 +1,5 @@
-import { pxToRem } from 'src/styles';
-import styled from 'styled-components';
+import { pxToRem, color } from 'src/styles';
+import styled, { css } from 'styled-components';
 
 export const ModalWrapper = styled.div`
   width: 100%;
@@ -24,36 +24,98 @@ export const ModalDeleteImg = styled.div`
   justify-content: flex-end;
 `;
 
-export const Img = styled.img`
+export const CloseModalImg = styled.img`
   width: 12px;
+
+  &:hover {
+    cursor: pointer;
+  }
 `;
 
-export const ModalTitle = styled.h3`
-  width: 100%;
+export const ModalTitle = styled.span`
   display: flex;
   justify-content: center;
+  width: 100%;
+  margin: 2.8rem 0 0.2rem 0;
+  font-size: 28px;
+  font-family: 'Noto Sans KR-Medium', sans-serif !important;
+  font-weight: bold;
 `;
 
-export const ModalSubTitle = styled.h5`
-
+export const ModalSubTitle = styled.span`
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  font-size: 15px;
+  font-weight: bold;
 `;
 
 export const FormBox = styled.div`
-
+  width: 100%;
+  margin-top: 3.6rem;
+  text-align: center;
+  
+  > span {
+    margin-right: 8rem;
+    font-size: 13px;
+    font-weight: bold;
+    font-family: 'Noto Sans KR', sans-serif !important;
+    color: ${color.primaryColor}
+  }
 `;
 
 export const InputBox = styled.div`
-
+  display: flex;
+  width: 18rem;
+  height: 1rem;
+  margin: 0 auto;
+  padding: 0.8rem;
+  border-radius: 5px;
+  border: 1px solid ${color.inputBackground};
+  background-color: ${color.inputBackground};
 `;
 
 export const Input = styled.input`
-
+  width: 100%;
+  height: 100%;
+  background-color: ${color.inputBackground};
+  font-size: 14px;
+  font-weight: bold;
+  font-family: 'Noto Sans KR', sans-serif !important;
 `;
 
-export const DeleteButton = styled.div`
+export const CheckDisplayImg = styled.img`
+  width: 20px;
+  height: 20px;
 
+  &:hover {
+    cursor: pointer;
+  }
+`;
+
+export const DeleteButton = styled.div<{ disable: boolean }>`
+  width: 19.6rem;
+  height: 3.2rem;
+  margin: 0 auto;
+  margin-top: 2rem;
+  border-radius: 5px;
+  ${({ disable }) => 
+    disable 
+      ? css`
+          border: 1px solid ${color.primaryColor};
+          background-color: ${color.primaryColor};
+        `
+      : css`
+          border: 1px solid ${color.fail};
+          background-color: ${color.fail};
+        `};
 `;
 
 export const Button = styled.button`
-
+  width: 100%;
+  height: 100%;
+  color: white;
+  font-size: 18px;
+  font-weight: bold;
+  font-family: 'Noto Sans KR-Midium', sans-serif !important;
 `;
