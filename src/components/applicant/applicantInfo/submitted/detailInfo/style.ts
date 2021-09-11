ceb076f82ee1bb19cdafcca6bffd355523e561a7
Hyeonyee <jgh10070@naver.com>
2021-09-12 00:07:49 +0900

    [FIX] detailinfo style

--- src/components/applicant/applicantInfo/submitted/detailInfo/style.ts
+++ src/components/applicant/applicantInfo/submitted/detailInfo/style.ts
@@ -16,11 +16,12 @@ export const DetailInfoTable = styled.table`
     font-weight: normal;
     width: 85px;
     font-size: 12px;
+    font-family: 'Noto Sans KR', sans-serif !important;
     font-weight: bold;
 
     &:first-child {
       border-left: 0;
-      width: 70px;
+      width: 85px;
       height: 33px;
     }
   }
@@ -37,6 +38,43 @@ export const DetailInfoTable = styled.table`
     &:last-child {
       border-right: 0;
     }
+  }
+  div > span {
+    font-size: 12px;
+
+    &:last-child {
+      margin-left: 32px;
+    }
+  }
+`;
+
+export const QualificationexamDetailInfoTable = styled.table`
+  width: 100%;
+  min-width: 30rem;
+  height: 250px;
+  margin-top: 16px;
+  border-collapse: collapse;
+
+  th,
+  td {
+    border: 1.4px solid #f0f0f0;
+  }
+  th {
+    text-align: center;
+    font-weight: normal;
+    width: 150px;
+    font-size: 12px;
+    font-family: 'Noto Sans KR', sans-serif !important;
+    font-weight: bold;
+    border-left: 0;
+  }
+  td {
+    text-align: left;
+    padding-left: 50px;
+    font-weight: bold;
+    font-family: 'Noto Sans KR', sans-serif !important;
+    font-size: 13px;
+    border-right: 0;
   }
   div > span {
     font-size: 12px;
