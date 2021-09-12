1d45a493af430d52f673861fa67588a11ded5e15
Hyeonyee <jgh10070@naver.com>
2021-09-12 15:39:08 +0900

    [FIX] auth reducer 수정

--- src/data/modules/redux/reducer/auth/index.ts
+++ src/data/modules/redux/reducer/auth/index.ts
@@ -1,92 +1,33 @@
-// import IAuthState from './interface';
-// import { 
-//   IS_LOGIN, 
-//   authActionType,
-//   ERROR,
-//   SIGNIN_FAILURE,
-//   SIGNIN_SUCCESS, } from '../../action/auth';
-
-// const initState: IAuthState = {
-//   isLogin: localStorage.getItem('access_token') ? true : false,
-//   id: '',
-//   password: '',
-//   error: null
-// };
-
-// const authReducer = (state: IAuthState = initState, action: authActionType) => {
-//   switch (action.type) {
-//     case ERROR: {
-//       return {
-//         ...state,
-//         error: action.payload,
-//       };
-//     }
-//     case IS_LOGIN: {
-//       return {
-//         ...state,
-//         isLogin: action.payload,
-//       };
-//     }
-//     case SIGNIN_FAILURE: {
-//       return {
-//         ...state,
-//         error: action.payload,
-//       };
-//     }
-//     case SIGNIN_SUCCESS: {
-//       return {
-//         ...state,
-//       };
-//     }
-//     default: {
-//       return state;
-//     }
-//   }
-// };
-
-// export default authReducer;
-
-import { LOGIN_ASYNC, REFRESH_TOKEN_ASYNC, AuthActions } from '../../action/auth';
-import { returnApiResponseData } from '../';
-import { API_STATUS } from '../../../../api/index';
-
-export interface InitialState {
-  loginStatus: 0 | 200 | 201 | 400 | 401;
-  tokens: {
-    access_token: string;
-    refresh_token: string;
-  };
-}
-
-const initialState: InitialState = {
-  loginStatus: 0,
-  tokens: {
-    access_token: null,
-    refresh_token: null,
+import IAuthState from './interface';
+import { ACCESS_TOKEN, authActionType, IS_LOGIN } from '../../action/auth';
+const initState: IAuthState = {
+  isLogin: localStorage.getItem('access_token') ? true : false,
+  accessToken: '',
+  error: {
+    status: 0,
+    message: '',
+    type: '',
   },
 };
 
-const authReducer = (state = initialState, action: AuthActions) => {
+const ModalReducer = (state: IAuthState = initState, action: authActionType) => {
   switch (action.type) {
-    case LOGIN_ASYNC: {
-      return returnApiResponseData<InitialState>({
-        state,
-        statusName: API_STATUS.loginStatus,
-        payload: action.payload,
-        dataKeyName: 'tokens',
-      });
+    case ACCESS_TOKEN: {
+      return {
+        ...state,
+        accessToken: action.payload,
+      };
     }
-    case REFRESH_TOKEN_ASYNC: {
-      return returnApiResponseData<InitialState>({
-        state,
-        statusName: API_STATUS.refreshTokenStatus,
-        payload: action.payload,
-        dataKeyName: 'tokens',
-      });
+    case IS_LOGIN: {
+      return {
+        ...state,
+        isLogin: action.payload,
+      };
     }
-    default:
+    default: {
       return state;
+    }
   }
 };
 
-export default authReducer;
+export default ModalReducer;
