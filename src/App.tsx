97e4017629fb7781938feae864b5e009dea0d1c8
Hyeonyee <jgh10070@naver.com>
2021-09-12 15:50:54 +0900

    [FIX] App.tsx 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,15 @@
 import React from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
-
-import {
-  LoginContainer,
-  MainContainer,
-  ScheduleContainer,
-  StatisticsContainer,
-  ApplicantContainer,
-} from "./containers";
-import CheckToken from "./components/common/checkToken";
-import { useHeader } from "./hooks/default";
+import GlobalStyle from "./styles/GlobalStyle";
+import RootRouter from "./route";
+import { Provider } from "react-redux";
+import store from "./data/modules/store";
 
 const App = () => {
-  const Header = useHeader();
   return (
-    <BrowserRouter>
-      {Header}
-      <Switch>
-        <Route exact path="/login" component={LoginContainer} />
-        <CheckToken>
-          <Route exact path="/" component={MainContainer} />
-          <Route exact path="/total" component={StatisticsContainer} />
-          <Route exact path="/schedule" component={ScheduleContainer} />
-          <Route exact path="/applicant" component={ApplicantContainer} />
-        </CheckToken>
-      </Switch>
-    </BrowserRouter>
+    <Provider store={store}>
+      <GlobalStyle />
+      <RootRouter />
+    </Provider>
   );
 };
 
