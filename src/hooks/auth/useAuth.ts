95ab8c9b5c38bccb5f78e4d08fadbcbb789ee9c4
Hyeonyee <jgh10070@naver.com>
2021-09-12 15:37:53 +0900

    [FIX] useAuth 간결화

--- src/hooks/auth/useAuth.ts
+++ src/hooks/auth/useAuth.ts
@@ -1,66 +1,18 @@
-// import { useDispatch } from 'react-redux';
-// import { useSelectState } from '../default';
-// import { refreshToken, signin, setIsLogin } from '../../data/modules/redux/action/auth';
-// import { LoginPayload } from "../../data/api/apiTypes";
+import { useDispatch } from 'react-redux';
+import { useSelectState } from '../default';
+import { setAccessToken, setIsLogin } from '../../data/modules/redux/action/auth';
 
-// const useSignin = () => {
-//   const dispatch = useDispatch();
-//   const state = useSelectState().auth;
-//   const setState = {
-//     setIsLogin: (payload: boolean) => dispatch(setIsLogin(payload)),
-//     signin: (payload: LoginPayload) => {
-//       dispatch(signin(payload));
-//     },
-//     refreshToken: (callback: () => void) => {
-//       dispatch(refreshToken({ callback }));
-//     },
-//   };
-//   return {
-//     state,
-//     setState,
-//   };
-// };
-
-// export default useSignin;
-
-import { useCallback } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-
-import {
-  loginAction,
-  loginPayload,
-  setTokenAction,
-  setTokenPayload,
-  refreshTokenAction,
-  refreshTokenPayload
-} from '../../data/modules/redux/action/auth';
-import { AppState } from '../../data/modules/store';
-import { InitialState } from '../../data/modules/redux/reducer/auth';
-
-export const useAuth = () => {
-  const authStore = useSelector<AppState, InitialState>((state) => ({
-    loginStatus: state.auth.loginStatus,
-    tokens: state.auth.tokens,
-  }));
-
+const useAuth = () => {
   const dispatch = useDispatch();
-
-  const login = useCallback(
-    (payload: loginPayload) => dispatch(loginAction(payload)),
-    [dispatch],
-  );
-  
-  const setTokens = useCallback(
-    (payload: setTokenPayload) => dispatch(setTokenAction(payload)),
-    [dispatch],
-  );
-
-  const refreshtoken = useCallback(
-    (payload: refreshTokenPayload) => dispatch(refreshTokenAction(payload)),
-    [dispatch],
-  )
-
-  return { authStore, login, setTokens, refreshtoken };
+  const state = useSelectState().auth;
+  const setState = {
+    setAccessToken: (payload: string) => dispatch(setAccessToken(payload)),
+    setIsLogin: (payload: boolean) => dispatch(setIsLogin(payload)),
+  };
+  return {
+    state,
+    setState,
+  };
 };
 
 export default useAuth;
