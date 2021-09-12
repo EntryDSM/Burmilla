777467d919169d12e033b016408bc72e01c274f8
Hyeonyee <jgh10070@naver.com>
2021-09-12 15:46:28 +0900

    [FIX] HeaderContainer 추가

--- src/containers/index.ts
+++ src/containers/index.ts
@@ -3,3 +3,4 @@ export { default as MainContainer } from './main';
 export { default as ScheduleContainer } from './schedule';
 export { default as StatisticsContainer } from './statistics';
 export { default as ApplicantContainer } from './applicant';
+export { default as HeaderContainer } from './header';
