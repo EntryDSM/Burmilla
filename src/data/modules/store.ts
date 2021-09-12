8fd574d7300c5f3cfd094816fd7e1d0b4cef962c
Hyeonyee <jgh10070@naver.com>
2021-09-12 15:38:04 +0900

    [FIX] store 수정

--- src/data/modules/store.ts
+++ src/data/modules/store.ts
@@ -3,15 +3,12 @@ import { composeWithDevTools } from 'redux-devtools-extension';
 import createSagaMiddleware from 'redux-saga';
 
 import rootReducer from './redux/reducer';
-import sagas from './redux/middleware';
-
-// const composeEnhancers = (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+import rootSaga from './redux/middleware';
 
 const sagaMiddleWare = createSagaMiddleware();
-
-export type AppState = ReturnType<typeof rootReducer>;
-
 export const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(sagaMiddleWare)));
 
-sagaMiddleWare.run(sagas);
+sagaMiddleWare.run(rootSaga);
+
 export type ReducerType = ReturnType<typeof rootReducer>;
+export default store;
