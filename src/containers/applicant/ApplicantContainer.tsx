238a739bc81d153531b650795848d2b57dc00b0b
Hyeonyee <jgh10070@naver.com>
2021-09-13 09:05:50 +0900

    [FIX] ERROR 처리

--- src/containers/applicant/ApplicantContainer.tsx
+++ src/containers/applicant/ApplicantContainer.tsx
@@ -1,10 +1,21 @@
 import React, { FC, Suspense } from "react";
+import { useHistory } from "react-router";
 import { useApplicant } from "../../hooks/applicant";
 
 const Applicant = React.lazy(() => import("../../components/applicant"));
 
 const ApplicantContainer: FC = () => {
   const { state, setState } = useApplicant();
+
+  const history = useHistory();
+
+  React.useEffect(() => {
+    const errorStatus = state.error.status;
+    if (errorStatus === 401 || errorStatus === 404) {
+      alert("다시 로그인해주세요.");
+      history.push("/login");
+    }
+  }, []);
 
   React.useEffect(() => {
     setState.getApplicantsList(state.filters);
