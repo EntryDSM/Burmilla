c99d06f2fb59f7710cd7347717147740c774691f
Hyeonyee <jgh10070@naver.com>
2021-09-30 10:12:34 +0900

    [FIX] Statistics refreshToken

--- src/containers/statistics/StatisticsContainer.tsx
+++ src/containers/statistics/StatisticsContainer.tsx
@@ -1,11 +1,17 @@
 import React, { FC, Suspense } from "react";
 import { useHistory } from "react-router";
+import { clearStorage } from "../../utils/token";
+import { useAuth } from "../../hooks/auth";
+import { useSignIn } from "../../hooks/signin";
 import { useStatistics } from "../../hooks/statistics";
 
 const Statistics = React.lazy(() => import("../../components/statistics"));
 
 const StatisticsContainer: FC = () => {
   const statisticsState = useStatistics();
+  const authState = useAuth();
+  const signinState = useSignIn();
+  const history = useHistory();
   const {
     common_score,
     meister_score,
@@ -17,6 +23,22 @@ const StatisticsContainer: FC = () => {
   React.useEffect(() => {
     statisticsState.setState.getStatistics();
   }, []);
+
+  const refreshToken = () => {
+    signinState.setState.refreshToken(statisticsState.setState.getStatistics);
+  };
+
+  React.useEffect(() => {
+    const errorStatus = statisticsState.state.error.status;
+    if (errorStatus === 401 || errorStatus === 403 || errorStatus === 404) {
+      refreshToken();
+      clearStorage();
+    }
+  }, [statisticsState.state.error]);
+
+  React.useEffect(() => {
+    if (authState.state.isLogin) statisticsState.setState.getStatistics();
+  }, [authState.state.isLogin, history.location.pathname]);
 
   return (
     <Suspense fallback={<div>로딩중...</div>}>
