47036dbc37efb93bac681d57faf81b88c6fd39ba
Hyeonyee <jgh10070@naver.com>
2021-09-30 10:13:06 +0900

    [FIX] Applicant refreshToken

--- src/containers/applicant/ApplicantContainer.tsx
+++ src/containers/applicant/ApplicantContainer.tsx
@@ -1,13 +1,10 @@
 import React, { FC, Suspense } from "react";
-import { useHistory } from "react-router";
 import { useApplicant } from "../../hooks/applicant";
 
 const Applicant = React.lazy(() => import("../../components/applicant"));
 
 const ApplicantContainer: FC = () => {
   const { state, setState } = useApplicant();
-
-  const history = useHistory();
 
   React.useEffect(() => {
     setState.getApplicantsList(state.filters);
