946186e2a027e6f04d286f9288aa8e60b9f0e34f
Hyeonyee <jgh10070@naver.com>
2021-09-30 10:12:42 +0900

    [FIX] Schedule refreshToken

--- src/containers/schedule/ScheduleContainer.tsx
+++ src/containers/schedule/ScheduleContainer.tsx
@@ -1,10 +1,33 @@
 import React, { FC, Suspense } from "react";
+import { useAuth } from "../../hooks/auth";
+import { clearStorage } from "../../utils/token";
+import { useHistory } from "react-router";
+import { useSignIn } from "../../hooks/signin";
 import { useSchedule } from "../../hooks/schedule";
 
 const Schedule = React.lazy(() => import("../../components/schedule"));
 
 const ScheduleContainer: FC = () => {
   const { state, setState } = useSchedule();
+  const authState = useAuth();
+  const signinState = useSignIn();
+  const history = useHistory();
+
+  const refreshToken = () => {
+    signinState.setState.refreshToken(setState.getStatus);
+  };
+
+  React.useEffect(() => {
+    const errorStatus = state.error.status;
+    if (errorStatus === 401 || errorStatus === 403 || errorStatus === 404) {
+      refreshToken();
+      clearStorage();
+    }
+  }, [state.error]);
+
+  React.useEffect(() => {
+    if (authState.state.isLogin) setState.getStatus();
+  }, [authState.state.isLogin, history.location.pathname]);
 
   return (
     <Suspense fallback={<div>로딩중...</div>}>
