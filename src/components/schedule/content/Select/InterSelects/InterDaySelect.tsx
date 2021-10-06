bb910e1c0c12013f0aeeb73870877e888daf7b06
Hyeonyee <jgh10070@naver.com>
2021-10-06 23:30:55 +0900

    [FIX] date 추가, 서버 값 고정 시킴

--- src/components/schedule/content/Select/InterSelects/InterDaySelect.tsx
+++ src/components/schedule/content/Select/InterSelects/InterDaySelect.tsx
@@ -1,12 +1,15 @@
 import React, { FC } from "react";
 import * as S from "../../../style";
 import { select_off, select_on } from "../../../../../assets/schedule";
+import { processTimeType } from "../../../../../data/modules/redux/reducer/schedule/interface";
+import { INTERVIEW } from "src/data/modules/redux/reducer/schedule/scheduleConstance";
 
 interface Props {
+  date: Array<processTimeType>;
   setInterviewScheduleDay: (payload: number) => void;
 }
 
-const MonthSelect: FC<Props> = ({ setInterviewScheduleDay }) => {
+const MonthSelect: FC<Props> = ({ date, setInterviewScheduleDay }) => {
   const [active, setActive] = React.useState(false);
   const [disabled, setDisabled] = React.useState("normal");
   const onestToThirtyOnest = [...Array(31)].map((_, i) => i + 1);
@@ -39,7 +42,11 @@ const MonthSelect: FC<Props> = ({ setInterviewScheduleDay }) => {
   return (
     <S.Select disabled={disabled} onClick={onSelectClick}>
       <S.SelectContent>
-        <p>{getLocalStorage}</p>
+        <p>
+          {date
+            .filter((date) => date.type === INTERVIEW)[0]
+            .date.slice(8, 10) && getLocalStorage}
+        </p>
         {activeImg}
       </S.SelectContent>
       {active && (
