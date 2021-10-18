ee1d0b60b3ad3395dea4d79d7378547f8d9e2f5b
Hyeonyee <jgh10070@naver.com>
2021-10-18 22:11:30 +0900

    [FIX] total_submitted_applicant_count 추가해서 올림

--- src/data/modules/redux/action/statistics/index.ts
+++ src/data/modules/redux/action/statistics/index.ts
@@ -12,6 +12,7 @@ import {
 export const getStatisticsSuccess = (payload: {
   total_applicant_count: number;
   total_competition_rate: number;
+  total_submitted_applicant_count: number;
   common_score: CommonScoreDistribution;
   meister_score: SpecialScoreDistribution;
   social_score: SpecialScoreDistribution;
